6446116d1e451c050103f229318ca2d67dfc23b4
ZwitterKaneda <smentek@gmail.com>
2022-11-26 02:24:54 -0300

    GX2-470: Corrección de indentación

--- cypress/e2e/test/Suites/GX2-470/loginPage.cy.js
+++ cypress/e2e/test/Suites/GX2-470/loginPage.cy.js
@@ -10,25 +10,25 @@ describe('GX2-470 ✅SwagLabs | Account | Iniciar sesión', () => {
 		cy.url().should('contain', 'saucedemo')
 	})
 	it(login.TC1.name, () => {
-		LoginPage.typeUsername(login.TC1.username);
-		LoginPage.typePassword(login.TC1.password);
-        LoginPage.clickLoginButton();
-        LoginPage.elemento.url().should('contain', login.TC1.expected);
-    })
-    it(login.TC2.name, () => {
-		LoginPage.typeUsername(login.TC2.username);
-		LoginPage.typePassword(login.TC2.password);
-        LoginPage.clickLoginButton();
-        LoginPage.elemento.errorMessage().should('have.text', login.TC2.expected);
-    })
-    it(login.TC3.name, () => {
-		LoginPage.typeUsername(login.TC3.username);
-		LoginPage.typePassword(login.TC3.password);
-        LoginPage.clickLoginButton();
-        LoginPage.elemento.errorMessage().should('have.text', login.TC3.expected);
+		LoginPage.typeUsername(login.TC1.username)
+		LoginPage.typePassword(login.TC1.password)
+		LoginPage.clickLoginButton()
+		LoginPage.elemento.url().should('contain', login.TC1.expected)
 	})
-    it(login.TC4.name, () => {
-        LoginPage.clickLoginButton();
-        LoginPage.elemento.errorMessage().should('have.text', login.TC4.expected);
+	it(login.TC2.name, () => {
+		LoginPage.typeUsername(login.TC2.username)
+		LoginPage.typePassword(login.TC2.password)
+		LoginPage.clickLoginButton()
+		LoginPage.elemento.errorMessage().should('have.text', login.TC2.expected)
+	})
+	it(login.TC3.name, () => {
+		LoginPage.typeUsername(login.TC3.username)
+		LoginPage.typePassword(login.TC3.password)
+		LoginPage.clickLoginButton()
+		LoginPage.elemento.errorMessage().should('have.text', login.TC3.expected)
+	})
+	it(login.TC4.name, () => {
+		LoginPage.clickLoginButton()
+		LoginPage.elemento.errorMessage().should('have.text', login.TC4.expected)
 	})
 })

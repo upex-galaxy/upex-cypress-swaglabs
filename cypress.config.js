956196adbba0481fa957ed2cbb797f17d24953e2
Franco <franco.esteves@gmail.com>
2023-01-15 19:47:09 -0300

    Ultimando detalles para ser lanzado al espacio

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'cypress';
-import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
-import pkg from '@badeball/cypress-cucumber-preprocessor';
-const {addCucumberPreprocessorPlugin} = pkg;
-import {createEsbuildPlugin} from '@badeball/cypress-cucumber-preprocessor/esbuild.js';
-import { downloadFile } from 'cypress-downloadfile/lib/addPlugin.js';
+import { defineConfig } from 'cypress'
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
+import pkg from '@badeball/cypress-cucumber-preprocessor'
+const { addCucumberPreprocessorPlugin } = pkg
+import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild.js'
+import { downloadFile } from 'cypress-downloadfile/lib/addPlugin.js'
 
 async function setupNodeEvents(on, config) {
 	// This is required for the preprocessor to be able to generate JSON reports after each run, and more,
 	await addCucumberPreprocessorPlugin(on, config)
 
-	on('task', {downloadFile})
+	on('task', { downloadFile })
 
 	on(
 		'file:preprocessor',
@@ -46,51 +46,58 @@ export default defineConfig({
 		// Glob pattern to determine what test files to load:
 		specPattern: ['cypress/e2e/cucumber-test/Gherkin/*.feature', 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'],
 		// Use Cypress plugins:
-    	setupNodeEvents,
+		setupNodeEvents,
 		// baseUrl: ""
 	},
-	env:{
 
-		AdminUser:{
-
-			username: "Admin",
-			password: "admin123"
+	env: {
+		AdminUser: {
+			username: 'Admin',
+			password: 'admin123',
 		},
-		endpoint:{
-			authLogin: "/auth/login",
-			dashboardIndex: "/dashboard/index",
-			signUp: "https://coderbyte.com/sl" 
+		endpoint: {
+			authLogin: '/auth/login',
+			dashboardIndex: '/dashboard/index',
+			signUp: 'https://coderbyte.com/sl',
 		},
-		
 		user: {
-			username: "upexTesting",
-			email: "sai@upextesting.com",
-			password: "1234567"
+			username: 'upexTesting',
+			email: 'sai@upextesting.com',
+			password: '1234567',
 		},
-		swagLabsEP:{
-			inventory:"https://www.saucedemo.com/inventory.html",
-			cart:"https://www.saucedemo.com/cart.html",
-			checkoutOne:"https://www.saucedemo.com/checkout-step-one.html",
-			checkoutTwo:"https://www.saucedemo.com/checkout-step-two.html",
-			checkoutAll:"https://www.saucedemo.com/checkout-complete.html"
 		
-		},
-		ErrorEP:{
-			inventoryError:"Epic sadface: You can only access '/inventory.html' when you are logged in.",
-			cartError:"Epic sadface: You can only access '/cart.html' when you are logged in.",
-			checkoutOneError:"Epic sadface: You can only access '/checkout-step-one.html' when you are logged in.",
-			checkoutTwoError:"Epic sadface: You can only access '/checkout-step-two.html' when you are logged in.",
-			checkoutAllError:"Epic sadface: You can only access '/checkout-complete.html' when you are logged in."
-		
-		},
-		flagsErrorSL:{
-			lockedUser:"Epic sadface: Sorry, this user has been locked out.",
-			PassOrUserInv:"Epic sadface: Username and password do not match any user in this service",
-			UserNull:"Epic sadface: Username is required",
-			PassNull:"Epic sadface: Password is required",
+		swagLabs:{
+			endpoint: {
+				inventory: '/inventory.html',
+				cart: '/cart.html',
+				checkoutOne: '/checkout-step-one.html',
+				checkoutTwo: '/checkout-step-two.html',
+				checkoutAll: '/checkout-complete.html',
+			},
+			login:{
+				users: {
+					correctUser: 'standard_user',
+					correctPass: 'secret_sauce',
+					lockUser: 'locked_out_user',
+					problemUser:'problem_user',
+					passInv: 'invalid_password',
+					glitchUser:'performance_glitch_user',
+					userInv:'invalid_username',
+				},
+				errorMsg: {
+					inventoryError: "Epic sadface: You can only access '/inventory.html' when you are logged in.",
+					cartError: "Epic sadface: You can only access '/cart.html' when you are logged in.",
+					checkoutOneError: "Epic sadface: You can only access '/checkout-step-one.html' when you are logged in.",
+					checkoutTwoError: "Epic sadface: You can only access '/checkout-step-two.html' when you are logged in.",
+					checkoutAllError: "Epic sadface: You can only access '/checkout-complete.html' when you are logged in.",
+					lockedUser: 'Epic sadface: Sorry, this user has been locked out.',
+					PassOrUserInv: 'Epic sadface: Username and password do not match any user in this service',
+					UserNull: 'Epic sadface: Username is required',
+					PassNull: 'Epic sadface: Password is required',
+				},
+			}	
 			
-			
-		},
-	}
-	
+		}
+	}	
 })
+
--- cypress/e2e/Tests/Accounts/GX-6516-Login.cy.js
+++ cypress/e2e/Tests/Accounts/GX-6516-Login.cy.js
@@ -1,59 +1,59 @@
 import { Login } from '@pages/loginFranco.Page'
-const { inventoryError, cartError, checkoutOneError, checkoutTwoError, checkoutAllError } = Cypress.env('errorEP')
-const { inventory, cart, checkoutOne, checkoutTwo, checkoutAll } = Cypress.env('swagLabsEP')
-const { UserNull, PassNull, PassOrUserInv, lockedUser } = Cypress.env('flagsErrorSL')
+
+const {login, endpoint} = Cypress.env('swagLabs')
+
 
 describe('✅SwagLabs | Account | Iniciar sesión y BR de Accesos', () => {
 	beforeEach('Precondición: Situado en la página de Login', () => {
 		cy.visit('https://www.saucedemo.com/')
 		cy.url().should('contain', 'saucedemo')
 	})
-	it.only('6517 | TC1: Validar login con credenciales correctas standard_user.', () => {
-		Login.enterUsername('standard_user') //User Correcto
-		Login.enterPassword('secret_sauce') //Password Correcta
+	it('6517 | TC1: Validar login con credenciales correctas standard_user.', () => {
+		Login.enterUsername(login.users.correctUser) //User Correcto
+		Login.enterPassword(login.users.correctPass) //Password Correcta
 		Login.submitButton('#login-button')
-		cy.should('contain', inventory)
+		cy.url().should('contain', inventory)
 	})
 	it('6517 | TC2: Validar login con credenciales correctas problem_user.', () => {
-		Login.enterUsername('problem_user') //User Correcto
-		Login.enterPassword('secret_sauce') //Password Correcta
+		Login.enterUsername(login.users.problemUser) //User Correcto
+		Login.enterPassword(login.users.correctPass) //Password Correcta
 		Login.submitButton('#login-button')
-		cy.should('contain', inventory)
+		cy.url().should('contain', inventory)
 	})
 	it('6517 | TC3: Validar login con credenciales correctas performance_glitch_user', () => {
-		Login.enterUsername('performance_glitch_user') //User Correcto
-		Login.enterPassword('secret_sauce') //Password Correcta
+		Login.enterUsername(login.users.glitchUser) //User Correcto
+		Login.enterPassword(login.users.correctPass) //Password Correcta
 		Login.submitButton('#login-button')
-		cy.should('contain', inventory)
+		cy.url().should('contain', inventory)
 	})
 
 	it('6517 | TC4: Validar no iniciar sesion con cuenta bloqueada locked_out_user', () => {
-		Login.enterUsername('locked_out_user') //user incorrecta
-		Login.enterPassword('secret_sauce') //password incorrecta
+		Login.enterUsername(login.users.lockUser) //user incorrecta
+		Login.enterPassword(login.users.correctPass) //password incorrecta
 		Login.submitButton('#login-button')
 	})
 
 	it('6517 | TC5: Validar no iniciar sesion con usuario correcto y password incorrecta.', () => {
-		Login.enterUsername('standard_user') //user correcto
-		Login.enterPassword('asd12345') //password incorrecta
+		Login.enterUsername(login.users.correctUser) //user correcto
+		Login.enterPassword(login.users.passInv) //password incorrecta
 		Login.submitButton('#login-button')
 		Login.get.error().should('contain', PassOrUserInv)
 	})
 	it('6517 | TC6: Validar no iniciar sesion con usuario incorrecto y password correcta.', () => {
-		Login.enterUsername('usuario_incorrecto') //user incorrecto
-		Login.enterPassword('secret_sauce') //password correcto
+		Login.enterUsername(login.users.userInv) //user incorrecto
+		Login.enterPassword(login.users.correctPass) //password correcto
 		Login.submitButton('#login-button')
 		Login.get.error().should('contain', PassOrUserInv)
 	})
 	it('6517 | TC7: Validar no iniciar sesion con username valido y password null.', () => {
-		Login.enterUsername('standard_user') //user correcto
+		Login.enterUsername(login.users.correctUser) //user correcto
 		//password null
 		Login.submitButton('#login-button')
 		Login.get.error().should('contain', PassNull)
 	})
 	it('6517 | TC8: Validar no iniciar sesion con username null y password valido.', () => {
 		//User Null
-		Login.enterPassword('secret_sauce') //password correcto
+		Login.enterPassword(login.users.correctPass) //password correcto
 		Login.submitButton('#login-button')
 		Login.get.error().should('contain', UserNull)
 	})
@@ -69,7 +69,7 @@ describe('✅SwagLabs | Account | Iniciar sesión y BR de Accesos', () => {
 	})
 	it('6517 | TC11: Validar usuario ingresa al endpoint /cart.html', () => {
 		cy.visit(cart, { failOnStatusCode: false })
-		Login.get.error.should('contain', cartError)
+		Login.get.error().should('contain', cartError)
 	})
 
 	it('6517 | TC12: Validar usuario ingresa al endpoint /checkout-step-one.html', () => {

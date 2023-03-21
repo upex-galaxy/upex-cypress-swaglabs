851dd85e16aed953fad45145732441b1c33fb797
leonardo <leonardo.millan011@gmail.com>
2023-03-21 15:42:30 -0400

    GX-11192 first commit

---
+++ cypress/e2e/Tests/ShoppingCart/GX-11192-checkout.cy.js
@@ -0,0 +1,11 @@
+describe('GX-11192', () => {
+	beforeEach('Precondicion', () => {
+		cy.visit('https://www.saucedemo.com');
+		cy.get('#user-name').type('standard_user');
+		cy.get('#password').type('secret_sauce');
+		cy.get('#login-button').click();
+	});
+	it('TC1', () => {
+		cy.get('html');
+	});
+});

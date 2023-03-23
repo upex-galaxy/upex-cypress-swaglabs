5c19acd238131f08f8791b4e81e7d3fcdf3f2b95
leonardo <leonardo.millan011@gmail.com>
2023-03-22 23:18:42 -0400

    GX-11303 Avance del dia

--- cypress/e2e/Tests/ShoppingCart/gx-11303-Addproduct.cy.js
+++ cypress/e2e/Tests/ShoppingCart/gx-11303-Addproduct.cy.js
@@ -1,8 +1,25 @@
+import { login as Login } from '@pages/Login.Page';
+import { plp } from '@pages/PLP-SCP';
+const { login } = Cypress.env('swagLabs');
+const base = Cypress.env('baseUrl');
+
 describe('GX-11303', () => {
-	beforeEach('Precondicion', () => {
-		cy.visit();
+	beforeEach('Precondicion para estar en el PLP', () => {
+		cy.visit(base);
+		Login.enterUsername(login.users.correctUser);
+		Login.enterPassword(login.users.correctPass);
+		Login.submitLogin();
 	});
-	it('TC1', () => {
-		cy.get();
+	it('TC1: Agregar al carrito dos items random y verificar que el boton (Add to cart) Cambien a (Remove)', () => {
+		plp.AddTwoRandomItem();
+		plp.CheckChangeBtn();
+	});
+	it('TC2: Agregar al carrito dos items random y verificar que el icono (Cart) sume dos items (2)', () => {
+		plp.AddTwoRandomItem();
+		plp.CheckaddBttcart();
+	});
+	it('TC3: Validar que (Name) y (Price) es el mismo del item agregado en la seccion Cart', () => {
+		plp.ValidateNameItem();
 	});
 });
+//ses
--- cypress/support/pages/PLP-SCP.js
+++ cypress/support/pages/PLP-SCP.js
@@ -5,6 +5,11 @@ class PLP {
 		FRemove: () => cy.get('[class="btn btn_secondary btn_small btn_inventory"]').eq(0),
 		gotocart: () => cy.get('[class="shopping_cart_link"]'),
 		gotocheckout: () => cy.get('#checkout'),
+		RAdd: () => cy.get('[class="btn btn_primary btn_small btn_inventory"]'),
+		CRemove: () => cy.get('[class="btn btn_secondary btn_small btn_inventory"]'),
+		Rtitle: () => cy.get('[class="inventory_item_name"]'),
+		Rprice: () => cy.get('[class="inventory_item_price"]'),
+		ScpItem: () => cy.get('[class="cart_contents_container"]'),
 	};
 	//Metodos
 	Firstitem() {
@@ -21,6 +26,33 @@ class PLP {
 	}
 	Checkout() {
 		this.get.gotocheckout().click();
+	}
+	AddTwoRandomItem() {
+		const num = Math.round(Math.random() * 5);
+		this.get.RAdd().eq(num).click();
+		const num2 = Math.round(Math.random() * 4);
+		this.get.RAdd().eq(num2).click();
+	}
+	CheckChangeBtn() {
+		this.get.CRemove().should('have.length', '2');
+		this.get.CRemove().eq(0).should('be.visible');
+		this.get.CRemove().eq(1).should('be.visible');
+	}
+	CheckaddBttcart() {
+		this.get.gotocart().should('contain', '2');
+	}
+	ValidateNameItem() {
+		const num = Math.round(Math.random() * 5);
+		this.get
+			.Rtitle()
+			.eq(num)
+			.then(NameItem => {
+				const nameItem = NameItem.text();
+				cy.log(nameItem);
+				this.get.RAdd().eq(num).click();
+				this.get.gotocart().click();
+				this.get.ScpItem().contains(nameItem);
+			});
 	}
 }
 export const plp = new PLP();

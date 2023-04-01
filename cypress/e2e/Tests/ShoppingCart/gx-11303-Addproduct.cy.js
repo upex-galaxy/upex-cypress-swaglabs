6d54081628aeb5f6535ef96d99d401c877746c2a
leonardo <leonardo.millan011@gmail.com>
2023-04-01 10:04:27 -0400

    GX-11303 Actualizacion y ultimas correcciones

--- cypress/e2e/Tests/ShoppingCart/gx-11303-Addproduct.cy.js
+++ cypress/e2e/Tests/ShoppingCart/gx-11303-Addproduct.cy.js
@@ -17,11 +17,23 @@ describe('GX-11303', () => {
 		ProductListpage.AddtoCartItemRandom(); //first Item
 		ProductListpage.AddtoCartItemRandom(); //Second item
 		ProductListpage.get.Removeofcart().should('be.visible').its('length').and('eq', 2); //Validate that (exist, visible) 2 button remove
+		ProductListpage.get.Removeofcart().should('have.lengthOf', 2);
+		ProductListpage.get.Removeofcart().should('exist');
+		ProductListpage.get.Removeofcart().then(large => {
+			const large2 = large.length;
+			expect(large2).eql(2);
+			expect(large).to.have.lengthOf(2);
+		});
 	});
 	it('TC2: Agregar al carrito dos items random y verificar que el icono (Cart) sume dos items (2)', () => {
 		ProductListpage.AddtoCartItemRandom(); //first Item
 		ProductListpage.AddtoCartItemRandom(); //second item
 		ProductListpage.get.cart().contains('2');
+		ProductListpage.get.cart().should('have.text', 2);
+		ProductListpage.get.cart().then(cart => {
+			expect(cart).to.have.text(2);
+			expect(cart.text()).eql('2');
+		});
 	});
 	it('TC3: Validar que (Name y Price) es el mismo del item agregado en la seccion Cart', () => {
 		ProductListpage.GetRandomItem();
@@ -30,11 +42,22 @@ describe('GX-11303', () => {
 		ProductListpage.addtoCartItemRandom();
 		ProductListpage.GotoShopingCart();
 		ShoppingCart.get.Productname().should('contain', nameItem);
+		ShoppingCart.get.Productname().then(Name => {
+			expect(Name.text()).to.include(nameItem);
+		});
 		ShoppingCart.get.Productprice().should('contain', priceItem);
+		ShoppingCart.get.Productprice().then(Price => {
+			expect(Price.text()).to.include(priceItem);
+		});
 	});
 	it('TC4: Validar agregar item desde el PDP y que sume 1+ en el icono (Cart)', function () {
 		ProductListpage.SelectrandomItem();
 		productDetailPage.AddItemToCart();
 		productDetailPage.get.cart().contains(1);
+		ProductListpage.get.cart().should('have.text', 1);
+		ProductListpage.get.cart().then(cart => {
+			expect(cart).to.have.text(1);
+			expect(cart.text()).eql('1');
+		});
 	});
 });

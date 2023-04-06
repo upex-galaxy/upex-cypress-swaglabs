ed41ca84a3366f365e53dd30779a9212cc65e3c6
Lucas Casco <lucassebaacasco@gmail.com>
2023-04-06 16:41:41 -0300

    GX-10740 modificacion de nombre de los TC

--- cypress/e2e/Tests/ShoppingCart/GX-10740-formularioCheckout.cy.js
+++ cypress/e2e/Tests/ShoppingCart/GX-10740-formularioCheckout.cy.js
@@ -17,7 +17,7 @@ describe('SwagLabs | Checkout Info | Insertar información del comprador.', () =
 
 	//agregar producto aleatorio al carrito
 
-	it('GX-10740|TC01 validar que el usuario ingresa exitosamente a la sección checkout-step-two', () => {
+	it('10741 | TC01 validar que el usuario ingresa exitosamente a la sección checkout-step-two', () => {
 		formCheck.typeName(formCheckOut.firstName);
 		formCheck.typeLastName(formCheckOut.lastName);
 		formCheck.typeCP(formCheckOut.zipCode);
@@ -25,34 +25,34 @@ describe('SwagLabs | Checkout Info | Insertar información del comprador.', () =
 		cy.url().should('contain', 'checkout-step-two');
 	});
 
-	it('GX-10740|TC02 validar first firstName null en formulario chechout', () => {
+	it('10741 | TC02 validar first firstName null en formulario chechout', () => {
 		formCheck.typeLastName(formCheckOut.lastName);
 		formCheck.typeCP(formCheckOut.zipCode);
 		formCheck.ClickBtnContinuo();
 		cy.get('[data-test="error"]').should('contain', 'Error: First Name is required');
 	});
 
-	it('GX-10740|TC03 validar last firstName null en formulario chechout', () => {
+	it('10741 | TC03 validar last firstName null en formulario chechout', () => {
 		formCheck.typeName(formCheckOut.firstName);
 		formCheck.typeCP(formCheckOut.zipCode);
 		formCheck.ClickBtnContinuo();
 		cy.get('[data-test="error"]').should('contain', 'Error: Last Name is required');
 	});
 
-	it.skip('GX-10740|TC04 validar caracteres especiales en formulario checkout', () => {
+	it.skip('10741 | TC04 validar caracteres especiales en formulario checkout', () => {
 		cy.get('[data-test="firstName"]').type('%%');
 		cy.get('[data-test="lastName"]').type('$$');
 		cy.get('[data-test="postalCode"]').type('··%');
 		cy.get('[data-test="error"]').should('contain', 'First_Name inválido');
 	});
 
-	it.skip('GX-10740|TC05 validar mensajes ingreso de caracteres numericos en campos first y last firstName del formulario checkout', () => {
+	it.skip('10741 | TC05 validar mensajes ingreso de caracteres numericos en campos first y last firstName del formulario checkout', () => {
 		cy.get('[data-test="lastName"]').type(1);
 		cy.get('[data-test="postalCode"]').type(2);
 		cy.get('[data-test="error"]').should('contain', 'Last_Name inválido');
 	});
 
-	it('GX-10740|TC06 validar regreso a seccion cart al hacer click en boton cancel del formulario chechout', () => {
+	it('10741 | TC06 validar regreso a seccion cart al hacer click en boton cancel del formulario chechout', () => {
 		formCheck.typeName(formCheckOut.firstName);
 		formCheck.typeCP(formCheckOut.zipCode);
 		formCheck.ClickBtnContinuo();

60a35852eab97438e114959923f62727b636abc5
leonardo <leonardo.millan011@gmail.com>
2023-04-10 21:46:46 -0400

    GX-11521 Update

--- cypress/e2e/Tests/Accounts/GX-11521-LoginForm.cy.js
+++ cypress/e2e/Tests/Accounts/GX-11521-LoginForm.cy.js
@@ -16,51 +16,51 @@ describe('GX-11521', () => {
 		Login.enterUsername(login.users.lockUser);
 		Login.enterPassword(login.users.correctPass);
 		Login.submitLogin();
-		Login.get.ErrorMsj().contains(login.errorMsg.lockedUser);
+		Login.get.ErrorMsgContainer().contains(login.errorMsg.lockedUser);
 	});
 	it('TC3: Validar No iniciar sesión con una cuenta inexistente.', () => {
 		Login.enterUsername(login.users.userInv);
 		Login.enterPassword(login.users.passInv);
 		Login.submitLogin();
-		Login.get.ErrorMsj().contains(login.errorMsg.PassOrUserInv);
+		Login.get.ErrorMsgContainer().contains(login.errorMsg.PassOrUserInv);
 	});
 	it('TC4: Validar No iniciar sesión con campo Username vacío.', () => {
 		Login.enterPassword(login.users.passInv);
 		Login.submitLogin();
-		Login.get.ErrorMsj().contains(login.errorMsg.UserNull);
+		Login.get.ErrorMsgContainer().contains(login.errorMsg.UserNull);
 	});
 	it('TC5: Validar No iniciar sesión con campo Password vacío.', () => {
 		Login.enterUsername(login.users.userInv);
 		Login.submitLogin();
-		Login.get.ErrorMsj().contains(login.errorMsg.PassNull);
+		Login.get.ErrorMsgContainer().contains(login.errorMsg.PassNull);
 	});
 	it('TC6: Validar No iniciar sesión con campo Username y Password vacío.', () => {
 		Login.submitLogin();
-		Login.get.ErrorMsj().contains(login.errorMsg.UserNull);
+		Login.get.ErrorMsgContainer().contains(login.errorMsg.UserNull);
 	});
 	it('TC7: Validar Mensaje no autorizado para endpoint /inventory.html', () => {
 		cy.visit(base + endpoint.inventory, { failOnStatusCode: false });
 		cy.url().should('contain', base);
-		Login.get.ErrorMsj().contains(login.errorMsg.inventoryError);
+		Login.get.ErrorMsgContainer().contains(login.errorMsg.inventoryError);
 	});
 	it('TC8: Validar Mensaje no autorizado para endpoint /cart.html', () => {
 		cy.visit(base + endpoint.cart, { failOnStatusCode: false });
 		cy.url().should('contain', base);
-		Login.get.ErrorMsj().contains(login.errorMsg.cartError);
+		Login.get.ErrorMsgContainer().contains(login.errorMsg.cartError);
 	});
 	it('TC9: Validar Mensaje no autorizado para endpoint /checkout-step-one.html', () => {
 		cy.visit(base + endpoint.checkoutOne, { failOnStatusCode: false });
 		cy.url().should('contain', base);
-		Login.get.ErrorMsj().contains(login.errorMsg.checkoutOneError);
+		Login.get.ErrorMsgContainer().contains(login.errorMsg.checkoutOneError);
 	});
 	it('TC10: Validar Mensaje no autorizado para endpoint /checkout-step-two.html', () => {
 		cy.visit(base + endpoint.checkoutTwo, { failOnStatusCode: false });
 		cy.url().should('contain', base);
-		Login.get.ErrorMsj().contains(login.errorMsg.checkoutTwoError);
+		Login.get.ErrorMsgContainer().contains(login.errorMsg.checkoutTwoError);
 	});
 	it('TC11: Validar Mensaje no autorizado para endpoint /checkout-complete.html', () => {
 		cy.visit(base + endpoint.checkoutAll, { failOnStatusCode: false });
 		cy.url().should('contain', base);
-		Login.get.ErrorMsj().contains(login.errorMsg.checkoutAllError);
+		Login.get.ErrorMsgContainer().contains(login.errorMsg.checkoutAllError);
 	});
 });

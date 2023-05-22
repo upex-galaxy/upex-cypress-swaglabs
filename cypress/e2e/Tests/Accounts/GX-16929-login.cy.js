18ce88cf5394472382c7c1e8767d7f3e9ff66b64
dbquiroga <day@xcapit.com>
2023-05-22 09:39:44 -0300

    cambios en test cy.js y POM, quitando aserciones del POM

--- cypress/e2e/Tests/Accounts/GX-16929-login.cy.js
+++ cypress/e2e/Tests/Accounts/GX-16929-login.cy.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
 import { LoginQuir } from '@pages/loginQuir.page';
 const {
-	swagLabs: { login },
+	swagLabs: { login, endpoint },
+	baseUrl,
 } = Cypress.env();
 
 describe('Iniciar Sesión', () => {
@@ -13,25 +14,48 @@ describe('Iniciar Sesión', () => {
 			loginData = data;
 		});
 	});
+
 	it('usuario intenta iniciar sesión con cuenta bloqueada', () => {
 		loginPage.login(loginData.data.Lockeduser, loginData.data.password);
 		cy.get(loginData.error).should('contain', login.errorMsg.lockedUser);
 	});
+
 	it('usuario intenta iniciar sesión con un cuenta incorrecta o inexistente', () => {
-		loginPage.login('anInvalidUser', loginData.data.password);
+		loginPage.login(login.users.userInv, loginData.data.password);
 		cy.get(loginData.error).should('contain', login.errorMsg.PassOrUserInv);
 	});
+
 	it('usuario intenta iniciar sesión dejando campos vacíos en el formulario', () => {
 		loginPage.loginWithEmptyFields('', loginData.data.password);
+		cy.get(loginData.error).should('exist').and('contain', login.errorMsg.UserNull);
+
 		loginPage.loginWithEmptyFields(loginData.data.user, '');
+		cy.get(loginData.error).should('exist').and('contain', login.errorMsg.PassNull);
+
 		loginPage.loginWithEmptyFields('', '');
+		cy.get(loginData.error).should('exist').and('contain', login.errorMsg.UserNull);
 	});
 
 	it('usuario inicia sesión correctamente', () => {
-		cy.Login(loginData.data.user, loginData.data.password);
+		loginPage.login(loginData.data.user, loginData.data.password);
+		loginPage.login(loginData.data.glitchuser, loginData.data.password);
+		loginPage.login(loginData.data.buguser, loginData.data.password);
 	});
 
 	it('usuario intenta ingresar a un endpoint de la website sin haber iniciado sesión', () => {
-		loginPage.verifyUnauthorizedAccess('https://www.saucedemo.com/inventory.html', login.errorMsg.inventoryError);
+		loginPage.verifyUnauthorizedAccess(baseUrl + endpoint.inventory);
+		cy.get(loginData.error).should('contain', login.errorMsg.inventoryError);
+
+		loginPage.verifyUnauthorizedAccess(baseUrl + endpoint.cart);
+		cy.get(loginData.error).should('contain', login.errorMsg.cartError);
+
+		loginPage.verifyUnauthorizedAccess(baseUrl + endpoint.checkoutOne);
+		cy.get(loginData.error).should('contain', login.errorMsg.checkoutOneError);
+
+		loginPage.verifyUnauthorizedAccess(baseUrl + endpoint.checkoutTwo);
+		cy.get(loginData.error).should('contain', login.errorMsg.checkoutTwoError);
+
+		loginPage.verifyUnauthorizedAccess(baseUrl + endpoint.checkoutAll);
+		cy.get(loginData.error).should('contain', login.errorMsg.checkoutAllError);
 	});
 });
--- cypress/support/pages/loginQuir.page.js
+++ cypress/support/pages/loginQuir.page.js
@@ -1,14 +1,13 @@
 export class LoginQuir {
 	constructor() {
 		this.baseUrl = Cypress.env().baseUrl;
-		this.errorUserNull = Cypress.env('swagLabs').login.errorMsg.UserNull;
-		this.errorPassNull = Cypress.env('swagLabs').login.errorMsg.PassNull;
 		this.user = '#user-name';
 		this.pass = '#password';
 		this.loginButton = '#login-button';
 		this.logOutButton = '#logout_sidebar_link';
 		this.submitButton = '[type= "submit"]';
 		this.error = '[data-test="error"]';
+		this.inventoryList = '.inventory_list';
 	}
 
 	login(username, password) {
@@ -23,33 +22,17 @@ export class LoginQuir {
 		cy.visit(this.baseUrl);
 		cy.url().should('contain', 'saucedemo');
 
-		const enterCredentialsAndSubmit = () => {
-			if (username !== '') {
-				cy.get(this.user).clear().type(username);
-			}
-			if (password !== '') {
-				cy.get(this.pass).clear().type(password);
-			}
-			cy.get(this.submitButton).click();
-		};
-
-		if (username === '' && password === '') {
-			enterCredentialsAndSubmit();
-			cy.get(this.error).should('exist').and('contain', this.errorUserNull);
-		} else if (username === '') {
-			enterCredentialsAndSubmit();
-			cy.get(this.error).should('exist').and('contain', this.errorUserNull);
-		} else if (password === '') {
-			enterCredentialsAndSubmit();
-			cy.get(this.error).should('exist').and('contain', this.errorPassNull);
-		} else {
-			this.login(username, password);
+		if (username !== '') {
+			cy.get(this.user).clear().type(username);
 		}
+		if (password !== '') {
+			cy.get(this.pass).clear().type(password);
+		}
+		cy.get(this.submitButton).click();
 	}
 
-	verifyUnauthorizedAccess(endpoint, errorMessage) {
+	verifyUnauthorizedAccess(endpoint) {
 		cy.visit(endpoint, { failOnStatusCode: false });
 		cy.location('href').should('contain', this.baseUrl);
-		cy.get(this.error).should('contain', errorMessage);
 	}
 }

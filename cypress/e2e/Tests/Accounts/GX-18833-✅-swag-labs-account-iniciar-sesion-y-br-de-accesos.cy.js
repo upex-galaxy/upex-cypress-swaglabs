700636821cf31fbd3b300edd6665a4b6d18acb49
Angel Mendez <mendez.angelleo@gmail.com>
2023-05-26 23:54:07 -0300

    GX-18833 fix de fixture

--- cypress/e2e/Tests/Accounts/GX-18833-✅-swag-labs-account-iniciar-sesion-y-br-de-accesos.cy.js
+++ cypress/e2e/Tests/Accounts/GX-18833-✅-swag-labs-account-iniciar-sesion-y-br-de-accesos.cy.js
@@ -1,18 +1,21 @@
-import { GX-18833.data.json } from './cypress/fixtures/data/GX-18833data.json';
+import data from 'cypress/fixtures/data/data.json';
+
+cy.log(data);
 
 describe('✅-swag-labs-account-iniciar-sesión-y-br-de-accesos', () => {
 	let username;
-	let l_username;
+	//let l_username;
 
 	before('fixture login', () => {
-		cy.fixture('C:galaxyL1-cypex-swaglabscypress\fixturesdataGX-18833data.json').then(data => {
+		cy.fixture('cypress/fixtures/data/data.json').then(data => {
 			username = data.username;
-			l_username = data.l_username;
+			//l_username = data.l_username;
 		});
 		cy.visit('https://www.saucedemo.com/');
 	});
 
-	it('GX-18834 | TC1: Validar que usuario pueda inciar sesion con el username standard_user exitosamente.', () => {
-		cy.get('#user-name').type('');
+	it('GX-18834 | TC1: Validar que usuario pueda iniciar sesión con el username standard_user exitosamente.', () => {
+		cy.get('#user-name').type(username); // Ingresar el valor del campo de nombre de usuario
+		// Agrega más pasos de prueba aquí
 	});
 });

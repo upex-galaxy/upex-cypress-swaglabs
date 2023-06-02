142bce7d5f0862068700da827363ff4ae12b6553
Hdzluis10 <hdzluis10@github.com>
2023-06-02 12:06:18 -0600

    GX-17548 Test and POM code enhancement all test passed

--- cypress/e2e/Tests/Accounts/GX-17548-Login-and-BRs.cy.js
+++ cypress/e2e/Tests/Accounts/GX-17548-Login-and-BRs.cy.js
@@ -1,50 +1,80 @@
-import { LoginBR } from 'cypress/support/pages/LoginBR.Page';
+import { Login } from 'cypress/support/pages/LoginBR.Page';
+
+import the from '@data/LoginAndBRs.json';
 
 describe('✅SwagLabs | Account | Iniciar sesión y BR de Accesos', () => {
+	// let infoData;
+
 	beforeEach('User must be in the Login page', () => {
 		cy.visit('/');
 		cy.url().should('contain', 'saucedemo');
 	});
 
 	it('17549 | TC1: Validate User can log in successfully', () => {
-		LoginBR.HappyPath();
+		Login.EnterData(the.dataValid.UsernameOK, the.dataValid.PasswordOK);
+		Login.get.usernameInput().should('have.value', the.dataValid.UsernameOK);
+		Login.get.passwordInput().should('have.value', the.dataValid.PasswordOK);
+		Login.Submit();
 		cy.url().should('contain', '/inventory.html');
 	});
+
 	it('17549 | TC2: Validate user cannot log in with empty data and BR5 is displayed', () => {
-		LoginBR.emptyFields();
+		Login.EnterData();
+		Login.Submit();
 		cy.get('.error-message-container.error').should('contain', 'Username is required');
 	});
 	it('17549 | TC3: Validate user cannot log in with Only number in Username field and BR2 is displayed', () => {
-		LoginBR.UserNumberOnly();
+		Login.EnterData(the.DataInvalid.username.User_Num, the.dataValid.PasswordOK);
+		Login.get.usernameInput().should('have.value', the.DataInvalid.username.User_Num);
+		Login.get.passwordInput().should('have.value', the.dataValid.PasswordOK);
+		Login.Submit();
 		cy.get('.error-message-container.error').should('contain', 'Username and password do not match any user in this service');
 	});
 	it('17549 | TC4: Validate user cannot log in with Especial Characters in Username field and BR2 is displayed', () => {
-		LoginBR.UserEspCharacterOnly();
+		Login.EnterData(the.DataInvalid.username.User_Esp, the.dataValid.PasswordOK);
+		Login.get.usernameInput().should('have.value', the.DataInvalid.username.User_Esp);
+		Login.get.passwordInput().should('have.value', the.dataValid.PasswordOK);
+		Login.Submit();
 		cy.get('.error-message-container.error').should('contain', 'Username and password do not match any user in this service');
 	});
 	it('17549 | TC5: Validate user cannot log in with invalid Username only and BR2 is displayed', () => {
-		LoginBR.invalidUserOnly();
+		Login.EnterData(the.DataInvalid.username.WrongUser, the.dataValid.PasswordOK);
+		Login.get.usernameInput().should('have.value', the.DataInvalid.username.WrongUser);
+		Login.get.passwordInput().should('have.value', the.dataValid.PasswordOK);
+		Login.Submit();
 		cy.get('.error-message-container.error').should('contain', 'Username and password do not match any user in this service');
 	});
 	it('17549 | TC6: Validate user cannot log in with invalid Password only and BR2 is displayed', () => {
-		LoginBR.invalidPswdOnly();
+		Login.EnterData(the.dataValid.UsernameOK, the.DataInvalid.password.WrongPassword);
+		Login.get.usernameInput().should('have.value', the.dataValid.UsernameOK);
+		Login.get.passwordInput().should('have.value', the.DataInvalid.password.WrongPassword);
+		Login.Submit();
 		cy.get('.error-message-container.error').should('contain', 'Username and password do not match any user in this service');
 	});
 	it('17549 | TC7: Validate user cannot log in with empty Username field only and BR3 is displayed ', () => {
-		LoginBR.emptyUserOnly();
+		Login.EnterData('', the.dataValid.PasswordOK);
+		Login.get.usernameInput().should('be.empty');
+		Login.get.passwordInput().should('have.value', the.dataValid.PasswordOK);
+		Login.Submit();
 		cy.get('.error-message-container.error').should('contain', 'Username is required');
 	});
 	it('17549 | TC8: Validate user cannot log in with empty Password field only and BR4 is displayed', () => {
-		LoginBR.emptyPswdOnly();
+		Login.EnterData(the.dataValid.UsernameOK, '');
+		Login.get.usernameInput().should('have.value', the.dataValid.UsernameOK);
+		Login.get.passwordInput().should('be.empty');
+		Login.Submit();
 		cy.get('.error-message-container.error').should('contain', 'Password is required');
 	});
 	it('17549 | TC9: Validate user cannot log in with Locked User credentials and BR1 is displayed', () => {
-		LoginBR.LockedUser();
+		Login.EnterData(the.LockedUSer, the.dataValid.PasswordOK);
+		Login.get.usernameInput().should('have.value', the.LockedUSer);
+		Login.get.passwordInput().should('have.value', the.dataValid.PasswordOK);
+		Login.Submit();
 		cy.get('.error-message-container.error').should('contain', 'Sorry, this user has been locked out.');
 	});
 
 	it('17549 | TC10: Validate User cannot access an Endpoint without Login', () => {
-		LoginBR.Endpoint();
+		Login.Endpoint();
 		cy.get('.error-message-container.error').should('contain', 'Epic sadface: You can only access');
 	});
 });
--- cypress/support/pages/LoginBR.Page.js
+++ cypress/support/pages/LoginBR.Page.js
@@ -3,71 +3,18 @@ class Session {
 		usernameInput: () => cy.get('#user-name'),
 		passwordInput: () => cy.get('#password'),
 		LoginBtn: () => cy.get('#login-button'),
-		// endpointURL: () => cy.visit('https://www.saucedemo.com/inventory.html/'),
-		btnError: () => cy.get('.error-message-container.error'),
+		// btnError: () => cy.get('.error-message-container.error'),
 	};
 
-	HappyPath() {
-		cy.fixture('data/LoginAndBRs').then(the => {
-			this.get.usernameInput().type(the.dataValid.UsernameOK);
-			this.get.passwordInput().type(the.dataValid.PasswordOK);
-			this.get.LoginBtn().click();
-		});
+	EnterData(username, password) {
+		username && this.get.usernameInput().type(username);
+		password && this.get.passwordInput().type(password);
 	}
-	emptyFields() {
+
+	Submit() {
 		this.get.LoginBtn().click();
 	}
 
-	UserNumberOnly() {
-		cy.fixture('data/LoginAndBRs').then(the => {
-			this.get.usernameInput().type(the.DataInvalid.username.User_Num);
-			this.get.passwordInput().type(the.dataValid.PasswordOK);
-			this.get.LoginBtn().click();
-		});
-	}
-	UserEspCharacterOnly() {
-		cy.fixture('data/LoginAndBRs').then(the => {
-			this.get.usernameInput().type(the.DataInvalid.username.User_Esp);
-			this.get.passwordInput().type(the.dataValid.PasswordOK);
-			this.get.LoginBtn().click();
-		});
-	}
-	invalidUserOnly() {
-		cy.fixture('data/LoginAndBRs').then(the => {
-			this.get.usernameInput().type(the.DataInvalid.username.WrongUser);
-			this.get.passwordInput().type(the.dataValid.PasswordOK);
-			this.get.LoginBtn().click();
-		});
-	}
-	invalidPswdOnly() {
-		cy.fixture('data/LoginAndBRs').then(the => {
-			this.get.usernameInput().type(the.dataValid.UsernameOK);
-			this.get.passwordInput().type(the.DataInvalid.password.WrongPassword);
-			this.get.LoginBtn().click();
-		});
-	}
-	emptyUserOnly() {
-		cy.fixture('data/LoginAndBRs').then(the => {
-			this.get.passwordInput().type(the.dataValid.PasswordOK);
-			this.get.LoginBtn().click();
-			//Username is required
-		});
-	}
-	emptyPswdOnly() {
-		cy.fixture('data/LoginAndBRs').then(the => {
-			this.get.usernameInput().type(the.dataValid.UsernameOK);
-			this.get.LoginBtn().click();
-			//Password is required
-		});
-	}
-	LockedUser() {
-		cy.fixture('data/LoginAndBRs').then(the => {
-			this.get.usernameInput().type(the.LockedUSer);
-			this.get.passwordInput().type(the.dataValid.PasswordOK);
-			this.get.LoginBtn().click();
-			//Password is required
-		});
-	}
 	Endpoint() {
 		cy.visit('https://www.saucedemo.com/inventory.html', {
 			failOnStatusCode: false,
@@ -75,4 +22,4 @@ class Session {
 	}
 }
 
-export const LoginBR = new Session();
+export const Login = new Session();

19a41f8f981db20e610ede01414e415d546be3c0
Wilson Medina <wilson.89medina@gmail.com>
2023-06-15 15:04:29 -0400

    tests: fixed the functions in the POM

--- cypress/support/pages/pom-GX-19828.js
+++ cypress/support/pages/pom-GX-19828.js
@@ -6,64 +6,61 @@ class PlpAndPdp {
         inputPassword : ()=> cy.get('[type="password"]'),
         buttonLogin: () => cy.get('[type="submit"]'),
         getListProducts: () => cy.get('[class$="_list"]'),
-        buttonSelectProduct: () => cy.get('button'),
+        buttonAddToCart: () => cy.get('[class*="btn_primary"]'),
+        buttonRemove: () => cy.get('[class*="btn_secondary"]'),
         linkCartIcon: () => cy.get('[class$="link"]'),
         buttonBackTo: () => cy.get('[name$="-products"]'),
-        getNameProduct: () => cy.get('[class$="desc_label"]'),
-        buttonContinueShopping: () => cy.get('[name$="shopping"]')
-
+        getNameProduct: () => cy.get('[class$="name"]'),
+        getNameProdct: () => cy.get('[class$="desc_label"]')
+        
     }
 
-    selectProduct(numProductoToSelect) {
-        this.element.getListProducts().children().then(products => {
-            const totalProducts = products.length;
-            const items = []
-		    while (items.length < numProductoToSelect) {
-		    	let selectProductRandom = Math.floor(Math.random() * totalProducts);
-		    	if (items.every(item => item !== selectProductRandom)) {
-		    		items.push(selectProductRandom)
+    addProduct(numProductoToAdd) {
+        let arrayNames = []
+        return this.element.getListProducts().children().then(products => {
+            let index = []
+		    while (index.length < numProductoToAdd) {
+		    	let randomNum = Math.floor(Math.random() * products.length);
+                if (index.every(i => i !== randomNum)) {
+                    cy.wrap(products).eq(randomNum).within(() => {
+                        this.element.getNameProduct().invoke('text').as('nameText')
+                        cy.get('@nameText').then(name => { arrayNames.push(name) })
+                        this.element.buttonAddToCart().click();
+                    })
+		    		index.push(randomNum)
 		    	}
-            }
-		    for (const item of items) {
-		        cy.wrap(products).eq(item).within(() => {
-		    		plpAndPdp.element.buttonSelectProduct().click({ multiple: true });
-                })
-            }
+            } 
+        }).then(() => {
+            return arrayNames
         })
     }
 
-    removeProductPlp(numProductoToDelete, numProductoSelected) {
-        for (let i = 0; i < numProductoToDelete; i++) {
-            cy.url().should('contain', '/inventory.html')
-            this.element.buttonSelectProduct().contains('Remove').parents('[class$="description"]').find('[class$=name]').then(names => {
-                let name = names.text()
-                this.element.buttonSelectProduct().contains('Remove').click({ multiple: true })
-                this.element.linkCartIcon().should('contain', numProductoSelected - 1 || '')
-                numProductoSelected--
-                this.element.linkCartIcon().click()
-                this.element.getNameProduct().should('not.have.text', name)
-                this.element.buttonContinueShopping().click()
-            })
-        }
+    removeProductPlp(numProductoToDelete, arrayNames) {
+        let newArrayNames = []
+        return this.element.getNameProduct().then(allNames => {
+            for (let i = 0; i < numProductoToDelete; i++) {
+                let randomNum = Math.floor(Math.random() * arrayNames.length)
+                cy.wrap(allNames).contains(arrayNames[randomNum]).parents('[class$="description"]').find('button').click({ multiple: true })
+                newArrayNames.push(arrayNames.splice(randomNum, 1))  
+            }
+        }).then(() => {
+            return newArrayNames
+        })  
     }
-    
-    removeProductPdp(numProductoToDelete, numProductoSelected) {
-        for (let i = 0; i < numProductoToDelete; i++) {
-            const url = 'item.html?id='
-            this.element.buttonSelectProduct().contains('Remove').parents('[class$="description"]').find('[class$=name]').then(names => {
-                let name = names.text()
-                this.element.buttonSelectProduct().contains('Remove').parents('[class$="description"]').find('a').click({ multiple: true })
-                cy.url().should('contain', url)
-                this.element.linkCartIcon().should('contain', numProductoSelected)
-                this.element.buttonSelectProduct().contains('Remove').click({ multiple: true })
-                this.element.linkCartIcon().should('contain', numProductoSelected - 1 || '')
-                numProductoSelected--
+
+    removeProductPdp(numProductoToDelete, arrayNames) {
+        let newArrayNames = []
+        return this.element.getListProducts().then(() => {
+            for (let i = 0; i < numProductoToDelete; i++) {
+                let randomNum = Math.floor(Math.random() * arrayNames.length)
+                this.element.getNameProduct().contains(arrayNames[randomNum]).click({ multiple: true })
+                this.element.buttonRemove().last().click()
                 this.element.buttonBackTo().click()
-                this.element.linkCartIcon().click()
-                this.element.getNameProduct().should('not.have.text', name)
-                this.element.buttonContinueShopping().click()
-            })
-        }
+                newArrayNames.push(arrayNames.splice(randomNum, 1))  
+            }
+        }).then(() => {
+            return newArrayNames
+        })  
     }
 }
 

86a8ddb2270d6bb12afa3adfa0244a49d594bb09
TheNotoriousMAGG <maran0301@gmail.com>
2023-06-25 10:43:07 -0400

    GX-21318 all tc ready and tests passed

--- cypress/e2e/Tests/Accounts/GX-21318-login-saucedemo.cy.js
+++ cypress/e2e/Tests/Accounts/GX-21318-login-saucedemo.cy.js
@@ -8,53 +8,69 @@ describe('GX-21318-✅-swag-labs-account-iniciar-sesion-y-br-de-accesos', () => 
 		cy.get(the.userone.input).type(the.userone.data.valid);
 		cy.get(the.password.input).type(the.password.data.valid);
 		cy.get(the.loginbutton).click();
-		cy.get('#item_4_title_link').should('contain.text', 'Sauce Labs Backpack');
+		cy.get(the.assertpagetwo.input);
 	});
 	it('GX-21319 | TC2: Validate user can log in with username “problem_user” successfully.', () => {
 		cy.get(the.usertwo.input).type(the.usertwo.data.valid);
 		cy.get(the.password.input).type(the.password.data.valid);
 		cy.get(the.loginbutton).click();
-		cy.get('#item_4_title_link').should('contain.text', 'Sauce Labs Backpack');
+		cy.get(the.assertpagetwo.input);
 	});
 	it('GX-21319 | TC3: Validate user can log in with username “performance_glitch_user” successfully.', () => {
 		cy.get(the.userthree.input).type(the.userthree.data.valid);
 		cy.get(the.password.input).type(the.password.data.valid);
 		cy.get(the.loginbutton).click();
-		cy.get('#item_4_title_link').should('contain.text', 'Sauce Labs Backpack');
+		cy.get(the.assertpagetwo.input);
 	});
 	it('GX-21319 | TC4: Validate user can’t log in with username “locked_out_user”.', () => {
 		cy.get(the.userblocked.input).type(the.userblocked.data.valid);
 		cy.get(the.password.input).type(the.password.data.valid);
 		cy.get(the.loginbutton).click();
-		cy.get('.error').eq('2').should('have.text', 'Epic sadface: Sorry, this user has been locked out.');
+		cy.get(the.element).should('have.text', the.lockedmsg);
 	});
 	it('GX-21319 | TC5: Validate log in with username and password that is not on database.', () => {
 		cy.get(the.userone.input).type(the.userone.data.invalid);
 		cy.get(the.password.input).type(the.password.data.invalid);
 		cy.get(the.loginbutton).click();
-		cy.get('.error').eq('2').should('have.text', 'Epic sadface: Username and password do not match any user in this service');
+		cy.get(the.element).should('have.text', the.invalidmsg);
 	});
 	it('GX-21319 | TC6: Validate log in with empty field on username and valid password.', () => {
 		cy.get(the.userone.input).should('be.empty');
 		cy.get(the.password.input).type(the.password.data.valid);
 		cy.get(the.loginbutton).click();
-		cy.get('.error').eq('2').should('have.text', 'Epic sadface: Username is required');
+		cy.get(the.element).should('have.text', the.usermsg);
 	});
 	it('GX-21319 | TC7: Validate log in with empty field on password and valid username.', () => {
 		cy.get(the.userone.input).type(the.userone.data.valid);
 		cy.get(the.password.input).should('be.empty');
 		cy.get(the.loginbutton).click();
-		cy.get('.error').eq('2').should('have.text', 'Epic sadface: Password is required');
+		cy.get(the.element).should('have.text', the.passwordmsg);
 	});
 	it('GX-21319 | TC8: Validate log in with empty field on username and password.', () => {
 		cy.get(the.userone.input).should('be.empty');
 		cy.get(the.password.input).should('be.empty');
 		cy.get(the.loginbutton).click();
-		cy.get('.error').eq('2').should('have.text', 'Epic sadface: Username is required');
+		cy.get(the.element).should('have.text', the.usermsg);
 	});
 	it('GX-21319 | TC9: Validate user can’t access endpoint “/inventory.html” without being logged in.', () => {
 		cy.visit('/inventory.html', { failOnStatusCode: false });
-		cy.get('[data-test="error"] button').should('exist');
+		cy.get(the.element).should('have.text', the.inventorymsg);
+	});
+	it('GX-21319 | TC10: Validate user can’t access endpoint “/cart.html” without being logged in.', () => {
+		cy.visit('/cart.html', { failOnStatusCode: false });
+		cy.get(the.element).should('have.text', the.cartmsg);
+	});
+	it('GX-21319 | TC11: Validate user can’t access endpoint “/checkout-step-one.html” without being logged in.', () => {
+		cy.visit('/checkout-step-one.html', { failOnStatusCode: false });
+		cy.get(the.element).should('have.text', the.steponemsg);
+	});
+	it('GX-21319 | TC12: Validate user can’t access endpoint “/checkout-step-two.html” without being logged in.', () => {
+		cy.visit('/checkout-step-two.html', { failOnStatusCode: false });
+		cy.get(the.element).should('have.text', the.steptwomsg);
+	});
+	it('GX-21319 | TC13: Validate user can’t access endpoint “/checkout-complete.html” without being logged in.', () => {
+		cy.visit('/checkout-complete.html', { failOnStatusCode: false });
+		cy.get(the.element).should('have.text', the.stepcompletemsg);
 	});
 });
 //Se importa la funcion

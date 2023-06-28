f6665ad9080ec23cef773d5bcb28e7f3f913427f
TheNotoriousMAGG <maran0301@gmail.com>
2023-06-27 20:22:45 -0400

    GX-21866 tc 1 2 3 4 done. Tc 5 in progress

--- cypress/e2e/Tests/ShoppingCart/GX-21866-AddtoCart.cy.js
+++ cypress/e2e/Tests/ShoppingCart/GX-21866-AddtoCart.cy.js
@@ -1,17 +1,50 @@
-import { the } from '../../../support/pages/GX-21866/AddToCart.Page';
-import fixture from '../../../fixtures/data/GX-21866/DataAddToCard.json';
-import { loginpage } from '/Github MAGG/L1-cypex-swaglabs/cypress.config';
+import { shoppingCart } from '@pages/GX-21866/AddToCart.Page';
+import the from '../../../fixtures/data/GX-21866/DataAddToCard.json';
 
 describe('GX-21866 | TS: ✅SwagLabs | SCP | Agregar producto al carrito de compras desde el PLP o PDP', () => {
 	beforeEach('Precondition: Having access to SUT and being logged in', () => {
 		cy.visit('/');
-		cy.url().should('contain', loginpage);
-		cy.get(fixture.username.input).type(fixture.username.data.valid);
-		cy.get(fixture.password.input).type(fixture.username.data.valid);
-		cy.get(fixture.loginbutton.input).click();
+		shoppingCart.get.loginassert().should('exist');
+		cy.get(the.username.input).type(the.username.data.valid);
+		cy.get(the.password.input).type(the.password.data.valid);
+		cy.get(the.loginbutton).click();
+		shoppingCart.get.titlemainpage().should('have.text', 'Products');
 	});
 
 	it('GX-21867| TC1: Validate user adds item from PLP to SC successfully.', () => {
-		the.additem();
+		shoppingCart.addbutton();
+		shoppingCart.get.scbadge().should('have.text', 1);
+		shoppingCart.carticon();
+		shoppingCart.get.continuebutton().should('have.text', 'Continue Shopping');
+	});
+
+	it('GX-21867| TC2: Validate user adds item from PDP to SC successfully.', () => {
+		shoppingCart.pickItemplp();
+		shoppingCart.get.backtoproducts().should('have.text', 'Back to products');
+		shoppingCart.addbutton();
+		shoppingCart.get.scbadge().should('have.text', 1);
+		shoppingCart.carticon();
+		shoppingCart.get.continuebutton().should('have.text', 'Continue Shopping');
+	});
+
+	it('GX-21867| TC3: Validate string of “Add to Cart" button changes to "Remove" from PLP.', () => {
+		shoppingCart.addbutton();
+		shoppingCart.get.scbadge().should('have.text', 1);
+		shoppingCart.removeItem();
+		shoppingCart.get.addbutton().should('have.text', 'Add to cart');
+	});
+
+	it('GX-21867| TC4: Validate string of “Add to Cart" button changes to "Remove" from PDP.', () => {
+		shoppingCart.pickItemplp();
+		shoppingCart.get.backtoproducts().should('have.text', 'Back to products');
+		shoppingCart.addbutton();
+		shoppingCart.get.scbadge().should('have.text', 1);
+		shoppingCart.removeItem();
+		shoppingCart.get.addbutton().should('have.text', 'Add to cart');
+	});
+
+	it('GX-21867| TC5: Validate item added from PLP has same characteristics on SCP.', () => {
+		shoppingCart.addbutton();
+		shoppingCart.get.scbadge().should('have.text', 1);
 	});
 });

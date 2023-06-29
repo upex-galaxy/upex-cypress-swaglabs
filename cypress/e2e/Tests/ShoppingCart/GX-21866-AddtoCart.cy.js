d2b6ff7cf0db66668f0a11c7831e84db957034ed
TheNotoriousMAGG <maran0301@gmail.com>
2023-06-28 21:17:44 -0400

    GX-21866 tc5 modified

--- cypress/e2e/Tests/ShoppingCart/GX-21866-AddtoCart.cy.js
+++ cypress/e2e/Tests/ShoppingCart/GX-21866-AddtoCart.cy.js
@@ -1,22 +1,50 @@
-import { shoppingCart } from '@pages/GX-21866/ProductDetailPage';
+import { PDP } from '@pages/GX-21866/ProductDetailPage';
 import the from '../../../fixtures/data/GX-21866/DataAddToCard.json';
 import { PLP } from '@pages/GX-21866/ProductListPage';
 
 describe('GX-21866 | TS: ✅SwagLabs | SCP | Agregar producto al carrito de compras desde el PLP o PDP', () => {
 	beforeEach('Precondition: Having access to SUT and being logged in', () => {
 		cy.visit('/');
-		shoppingCart.get.loginAssert().should('exist');
+		PDP.get.loginAssert().should('exist');
 		cy.get(the.username.input).type(the.username.data.valid);
 		cy.get(the.password.input).type(the.password.data.valid);
 		cy.get(the.loginbutton).click();
-		shoppingCart.get.titlemainpage().should('have.text', 'Products');
+		PDP.get.titlemainpage().should('have.text', 'Products');
 	});
 
-	it.only('GX-21867| TC1: Validate user adds item from PLP to SC successfully.', () => {
-		/* shoppingCart.addbutton();
-		shoppingCart.get.scbadge().should('have.text', 1);
-		shoppingCart.carticon();
-		shoppingCart.get.continuebutton().should('have.text', 'Continue Shopping'); */
+	it('GX-21867| TC1: Validate user adds item from PLP to SC successfully.', () => {
+		PDP.addbutton();
+		PDP.get.scbadge().should('have.text', 1);
+		PDP.carticon();
+		PDP.get.continuebutton().should('have.text', 'Continue Shopping');
+	});
+
+	it('GX-21867| TC2: Validate user adds item from PDP to SC successfully.', () => {
+		PDP.pickItemplp();
+		PDP.get.backtoproducts().should('have.text', 'Back to products');
+		PDP.addbutton();
+		PDP.get.scbadge().should('have.text', 1);
+		PDP.goToShoppingCart();
+		PDP.get.continuebutton().should('have.text', 'Continue Shopping');
+	});
+
+	it('GX-21867| TC3: Validate string of “Add to Cart" button changes to "Remove" from PLP.', () => {
+		PDP.addbutton();
+		PDP.get.scbadge().should('have.text', 1);
+		PDP.removeItem();
+		PDP.get.addbutton().should('have.text', 'Add to cart');
+	});
+
+	it('GX-21867| TC4: Validate string of “Add to Cart" button changes to "Remove" from PDP.', () => {
+		PDP.pickItemplp();
+		PDP.get.backtoproducts().should('have.text', 'Back to products');
+		PDP.addbutton();
+		PDP.get.scbadge().should('have.text', 1);
+		PDP.removeItem();
+		PDP.get.addbutton().should('have.text', 'Add to cart');
+	});
+
+	it.only('GX-21867| TC5: Validate item added from PLP has same characteristics on SCP.', () => {
 		let itemName;
 		let itemPrice;
 		PLP.get
@@ -28,39 +56,10 @@ describe('GX-21866 | TS: ✅SwagLabs | SCP | Agregar producto al carrito de comp
 				PLP.get.itemButton().click();
 			})
 			.then(() => {
-				shoppingCart.goToShoppingCart();
-				cy.log(itemName);
-				cy.log(itemPrice);
+				PDP.goToShoppingCart();
+				PDP.get.itemPlp().should('have.text', itemName);
+				PDP.get.itemPriceShoppingCart().should('have.text', itemPrice);
 			});
 	});
-
-	it('GX-21867| TC2: Validate user adds item from PDP to SC successfully.', () => {
-		shoppingCart.pickItemplp();
-		shoppingCart.get.backtoproducts().should('have.text', 'Back to products');
-		shoppingCart.addbutton();
-		shoppingCart.get.scbadge().should('have.text', 1);
-		shoppingCart.goToShoppingCart();
-		shoppingCart.get.continuebutton().should('have.text', 'Continue Shopping');
-	});
-
-	it('GX-21867| TC3: Validate string of “Add to Cart" button changes to "Remove" from PLP.', () => {
-		shoppingCart.addbutton();
-		shoppingCart.get.scbadge().should('have.text', 1);
-		shoppingCart.removeItem();
-		shoppingCart.get.addbutton().should('have.text', 'Add to cart');
-	});
-
-	it('GX-21867| TC4: Validate string of “Add to Cart" button changes to "Remove" from PDP.', () => {
-		shoppingCart.pickItemplp();
-		shoppingCart.get.backtoproducts().should('have.text', 'Back to products');
-		shoppingCart.addbutton();
-		shoppingCart.get.scbadge().should('have.text', 1);
-		shoppingCart.removeItem();
-		shoppingCart.get.addbutton().should('have.text', 'Add to cart');
-	});
-
-	it('GX-21867| TC5: Validate item added from PLP has same characteristics on SCP.', () => {
-		shoppingCart.addbutton();
-		shoppingCart.get.scbadge().should('have.text', 1);
-	});
+	it('GX-21867| TC6: Validate item added from PDP has same characteristics on SCP.', () => {});
 });
--- cypress/support/pages/GX-21866/ProductDetailPage.js
+++ cypress/support/pages/GX-21866/ProductDetailPage.js
@@ -1,16 +1,17 @@
-class ShoppingCart {
+class ProductDetailPage {
 	get = {
 		loginAssert: () => cy.get('#login_button_container'),
 		titlemainpage: () => cy.get('.title'),
 		addbutton: () => cy.get('#add-to-cart-sauce-labs-bolt-t-shirt'),
 		removebutton: () => cy.get('#remove-sauce-labs-bolt-t-shirt'),
 		cartIcon: () => cy.get('.shopping_cart_link'),
+		itemPriceShoppingCart: () => cy.get('.inventory_item_price'),
 		scbadge: () => cy.get('.shopping_cart_badge'),
-		itemplp: () => cy.get('#item_1_title_link'),
+		itemPlp: () => cy.get('.inventory_item_name'),
 		itempdp: () => cy.get('#inventory_item_container'),
 		descriptionitem: () => cy.get('.inventory_item_desc').eq(2),
 		photoitem: () => cy.get('.inventory_item_img').eq(2),
-		priceitem: () => cy.get('inventory_item_price').eq(2),
+		priceitem: () => cy.get('.inventory_item_price'),
 		descriptionitemSCP: () => cy.get('#inventory_details_desc'),
 		continuebutton: () => cy.get('[class^="btn btn_secondary back btn_medium"]'),
 		backtoproducts: () => cy.get('#back-to-products'),
@@ -28,13 +29,9 @@ class ShoppingCart {
 		this.get.removebutton().click();
 	}
 
-	pickItemplp() {
-		this.get.itemplp().click();
-	}
-
 	addItempdp() {
 		this.get.addbutton().click();
 	}
 }
 
-export const shoppingCart = new ShoppingCart();
+export const PDP = new ProductDetailPage();

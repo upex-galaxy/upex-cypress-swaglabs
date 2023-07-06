6818369262aef6edc62782d2caa2155c2a52d343
jorabdo <jorabdo1991@gmail.com>
2023-07-06 12:52:30 -0500

    GX-23444 | creando 2 TC usando forEach

---
+++ cypress/e2e/Tests/Accounts/GX-23444-AccountChallenge.cy.js
@@ -0,0 +1,51 @@
+import testCase from '../../../fixtures/DOM/space//accountChallenge.json';
+import endpoints from '../../../fixtures/DOM/space//endpoints.json';
+import { login } from '@pages/accountPage';
+const { baseUrl } = Cypress.env();
+
+describe('GX-22504 | SwagLabs | Account | Iniciar sesión y BR de Accesos', () => {
+	beforeEach('', () => {
+		cy.visit(baseUrl);
+	});
+	testCase.forEach(tests => {
+		it(tests.test, () => {
+			if (tests.test == 'LogIn successful') {
+				login.fillUsername(tests.username);
+				login.fillPassword(tests.password);
+				login.clickLoginBtn();
+				cy.url().should('contain', 'inventory.html');
+			} else if (tests.test == 'LogIn blocked') {
+				login.fillUsername(tests.username);
+				login.fillPassword(tests.password);
+				login.clickLoginBtn();
+				login.get.errorMsg().should('have.text', tests.msg);
+				cy.url().should('contain', baseUrl);
+			} else if (tests.test == 'LogIn Invalid') {
+				login.fillUsername(tests.username);
+				login.fillPassword(tests.password);
+				login.clickLoginBtn();
+				login.get.errorMsg().should('have.text', tests.msg);
+				cy.url().should('contain', baseUrl);
+			} else {
+				if (tests.username != '') {
+					login.get.username().type(tests.username);
+				}
+				if (tests.password != '') {
+					login.get.password().type(tests.password);
+				}
+				login.clickLoginBtn();
+				login.get.errorMsg().should('have.text', tests.msg);
+			}
+		});
+	});
+
+	endpoints.forEach(tests => {
+		it(tests.test, () => {
+			if (tests.test == 'No Authentication Access') {
+				cy.visit(baseUrl + tests.endpoint, { failOnStatusCode: false });
+				login.get.errorMsg().should('have.text', tests.msg);
+				cy.url().should('contain', baseUrl);
+			}
+		});
+	});
+});

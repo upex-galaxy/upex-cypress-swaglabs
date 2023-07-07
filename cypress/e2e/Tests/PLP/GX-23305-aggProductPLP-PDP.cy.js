ab28fcb83f47f24d929c9f3de88b0824ecf702f0
gualex89 <gualex89@gmail.com>
2023-07-06 22:01:42 -0400

    GX-23305 TC1 terminado

--- cypress/e2e/Tests/PLP/GX-23305-aggProductPLP-PDP.cy.js
+++ cypress/e2e/Tests/PLP/GX-23305-aggProductPLP-PDP.cy.js
@@ -1,17 +1,60 @@
-describe('23305 | ✅SwagLabs | SCP | Agregar producto al carrito de compras desde el PLP o PDP'), () =>
+describe('23305 | ✅SwagLabs | SCP | Agregar producto al carrito de compras desde el PLP o PDP', () =>
 {
     beforeEach('Usuario ha iniciado sesión y no ha sido agregado el producto al SCP', () => 
     {
         cy.fixture('data/GX-23305-aggProductPLP-PDP.json',).then((the) => {
             cy.visit('https://www.saucedemo.com/');
             cy.url().should('eq', 'https://www.saucedemo.com/')
-            ATC.get.usernameInput(the.data.username);
-            ATC.get.passwordInput(the.data.password);
-            ATC.get.submitBtn().click()
+            addToCart.enterUsername(the.data.username);
+            addToCart.enterPassword(the.data.password);
+            addToCart.get.submitBtn().click()
         })
-        
+    })
+    it('23306 | TC1: Validar agregar desde el PLP un artículo al shopping cart.', () =>
+    {
+        // Selects a random item and adds to cart
+        addToCart.selectRandomItem().then(indexselected => {
+            cy.wrap(indexselected).as('index');
+        })
 
+        // ------------------------------Results expected---------------------------------------------------
+
+        // checks if button was changed
+        cy.get('@index').then((index) => {
+            addToCart.get.addToCartItemBtn().eq(index).should('have.text', 'Remove');
+        })
+        // Checks if was added one to the shopping cart icon
+        addToCart.get.shoppingCartBadge().invoke('text').then(cantidad => {
+            
+            quantity = parseInt(cantidad);
+            expect(quantity).to.equal(1);
+        })
+        //
+        cy.get('@index').then((index) => {
+            addToCart.get.inventoryItemName().eq(index).invoke('text').then(itemName => {
+                cy.wrap(itemName).as('itemNameToCompare');
+            })
+            addToCart.get.inventoryItemDesc().eq(index).invoke('text').then(itemDescription => {
+                cy.wrap(itemDescription).as('itemDescriptionToCompare');
+            })
+            addToCart.get.inventoryItemPrice().eq(index).invoke('text').then(itemPrice => {
+                cy.wrap(itemPrice).as('itemPriceToCompare');
+            })
+        })
+        // click Shopping Cart Button
+        addToCart.get.shoppingCartBtn().click()
+        
+        // checks if the characteristics are match
+        cy.get('@itemNameToCompare').then((itemNameToCompare) => {
+            addToCart.get.inventoryItemName().should('have.text', itemNameToCompare);
+        })
+        cy.get('@itemDescriptionToCompare').then((itemDescriptionToCompare) => {
+            addToCart.get.inventoryItemDesc().should('have.text', itemDescriptionToCompare);
+        })
+        cy.get('@itemPriceToCompare').then((itemPriceToCompare) => {
+            addToCart.get.inventoryItemPrice().should('have.text', itemPriceToCompare);
+        })
     })
     
-}
-import { ATC } from "@pages/GX-23305-aggProductPLP-PDP.Page";
+})
+import { addToCart } from "@pages/GX-23305-aggProductPLP-PDP.Page";
--- cypress/support/pages/GX-23305-aggProductPLP-PDP.Page.js
+++ cypress/support/pages/GX-23305-aggProductPLP-PDP.Page.js
@@ -1,9 +1,31 @@
-class addToCart {
+class ATC {
     get = {
         usernameInput: () => cy.get('#user-name'),
         passwordInput: () => cy.get('#password'),
         submitBtn: () => cy.get('#login-button'),
-
+        addToCartItemBtn: () => cy.get('.btn_inventory'),
+        shoppingCartBadge: () => cy.get('.shopping_cart_badge'),
+        shoppingCartBtn: () => cy.get('#shopping_cart_container'),
+        inventoryItemName: () => cy.get('.inventory_item_name'),
+        inventoryItemDesc: () => cy.get('.inventory_item_desc'),
+        inventoryItemPrice: () => cy.get('.inventory_item_price'),
     }
+    enterUsername(value) {
+        this.get.usernameInput().type(value)
+    }
+    enterPassword(value) {
+        this.get.passwordInput().type(value)
+    }
+    selectRandomItem(){
+        let randomItem
+        return this.get.addToCartItemBtn().then(largo => {
+            randomItem = Cypress._.random(0, largo.length - 1);
+            this.get.addToCartItemBtn().eq(randomItem).click();
+        }).then(() => {
+            return randomItem
+        })
+        
+    }
+
 }
-export const ATC = new addToCart()
+export const addToCart  = new ATC()

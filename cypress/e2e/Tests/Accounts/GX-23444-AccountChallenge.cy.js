fed05b93ad565e077edaf9c49c26bc58da2b692e
jorabdo <jorabdo1991@gmail.com>
2023-07-09 18:28:15 -0500

    GX-23444 |limpiando y mejorando codigo y nomenclaturas

--- cypress/e2e/Tests/Accounts/GX-23444-AccountChallenge.cy.js
+++ cypress/e2e/Tests/Accounts/GX-23444-AccountChallenge.cy.js
@@ -1,49 +1,48 @@
 import testCase from '../../../fixtures/DOM/space/GX-23444-accountChallenge';
-import endpoints from '../../../fixtures/DOM/space/GX-23444-endpoints';
-import { login } from '@pages/accountPage';
+import { login } from '@pages/account'; // se utilizo el mismo Page que otras US.
 const { baseUrl } = Cypress.env();
+const { swagLabs } = Cypress.env();
 
 describe('GX-22504 | SwagLabs | Account | Iniciar sesión y BR de Accesos', () => {
-	beforeEach('User must be situated in the website', () => {
+	beforeEach(() => {
 		cy.visit(baseUrl);
 	});
-	testCase.forEach(tests => {
-		it(tests.test, () => {
-			if (tests.test == 'LogIn successful') {
+
+	testCase.test.forEach(tests => {
+		it(tests.name, () => {
+			const { name, username, password, msg } = tests;
+			if (name === '23455 | TC1: Validate LogIn when is successful.') {
 				login.fillUsername(tests.username);
 				login.fillPassword(tests.password);
 				login.clickLoginBtn();
-				cy.url().should('contain', 'inventory.html');
-			} else if (tests.test == 'LogIn blocked') {
-				login.fillUsername(tests.username);
-				login.fillPassword(tests.password);
+				cy.url().should('contain', swagLabs.endpoint.inventory);
+			} else if (name === '23455 | TC2: Validate LogIn when is blocked.') {
+				login.fillUsername(username);
+				login.fillPassword(password);
 				login.clickLoginBtn();
-				login.get.errorMsg().should('have.text', tests.msg);
+				login.get.errorMsg().should('have.text', msg);
 				cy.url().should('contain', baseUrl);
-			} else if (tests.test == 'LogIn Invalid') {
-				login.fillUsername(tests.username);
-				login.fillPassword(tests.password);
+			} else if (name === '23455 | TC3: Validate LogIn when is invalid.') {
+				login.fillUsername(username);
+				login.fillPassword(password);
 				login.clickLoginBtn();
-				login.get.errorMsg().should('have.text', tests.msg);
+				login.get.errorMsg().should('have.text', msg);
 				cy.url().should('contain', baseUrl);
 			} else {
-				if (tests.username != '') {
-					login.get.username().type(tests.username);
-				}
-				if (tests.password != '') {
-					login.get.password().type(tests.password);
-				}
+				login.emptyFields(username, password);
 				login.clickLoginBtn();
-				login.get.errorMsg().should('have.text', tests.msg);
+				login.get.errorMsg().should('have.text', msg);
 			}
 		});
 	});
 
-	endpoints.forEach(tests => {
-		it(tests.test, () => {
-			if (tests.test == 'No Authentication Access') {
-				cy.visit(baseUrl + tests.endpoint, { failOnStatusCode: false });
-				login.get.errorMsg().should('have.text', tests.msg);
+	testCase.endpoint.forEach(test => {
+		it(test.name, () => {
+			const { name, endpoint, msg } = test;
+
+			if (name === '23455 | TC7: Validate enter to an internal endpoint of the website without doing the LogIn before.') {
+				cy.visit(baseUrl + endpoint, { failOnStatusCode: false });
+				login.get.errorMsg().should('have.text', msg);
 				cy.url().should('contain', baseUrl);
 			}
 		});

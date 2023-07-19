0f8cf1e16fd87df4b42df8cb44123d55aeab3afc
JonatanDavidGauto <elchoni90@gmail.com>
2023-07-19 19:58:55 +0200

    GX-25146: Se creo el TC2, se agrego data al Fixture y se agrego selector al POM

--- cypress/e2e/Tests/Accounts/GX-25146-Login.cy.js
+++ cypress/e2e/Tests/Accounts/GX-25146-Login.cy.js
@@ -2,16 +2,25 @@ import { Login } from '@pages/GX-25146-Login.Page';
 
 import Credenciales from '@data/GX-25146-Login.json';
 
-const UrlHome = Credenciales.UrlHome;
+const UrlHome = Credenciales.Url.UrlHome;
+const BaseUrl = Credenciales.Url.BaseUrl;
+const BlockedMessage = Credenciales.Error.Blocked;
 
 describe('GX-25146-✅-swag-labs-account-iniciar-sesion-y-br-de-accesos', () => {
 	beforeEach('Usuario se encuentra en la Pagina de login', () => {
-		cy.visit('/');
+		cy.visit(BaseUrl);
 	});
 	it('25147 | TC1: Validar el inicio exitoso cuando se usa Data válida y correcta', () => {
-		Login.UserOK();
-		Login.PassOK();
-		Login.ButtonLogin();
-		cy.url().should('contain', UrlHome);
+		Login.TypeValidUser();
+		Login.TypeValidPass();
+		Login.ClickButtonLogin();
+		cy.url().should('contain', BaseUrl + UrlHome);
+	});
+	it('25147 | TC2: Validar el No inicio de sesión con un usuario bloqueado', () => {
+		Login.TypeBlockedUser();
+		Login.TypeValidPass();
+		Login.ClickButtonLogin();
+		Login.get.ErrorMessage().should('contain', BlockedMessage);
+		cy.url().should('contain', BaseUrl);
 	});
 });
--- cypress/support/pages/GX-25146-Login.Page.js
+++ cypress/support/pages/GX-25146-Login.Page.js
@@ -2,23 +2,29 @@ import Credenciales from '@data/GX-25146-Login.json';
 
 const UserOK = Credenciales.User.valid.Correct;
 const PassOK = Credenciales.Password.valid;
+const UserBlocked = Credenciales.User.invalid.blocked;
 
 class login {
 	get = {
 		UserInput: () => cy.get('[id="user-name"]'),
 		PassInput: () => cy.get('[id="password"]'),
 		LoginButton: () => cy.get('[id="login-button"]'),
+		ErrorMessage: () => cy.get('[class="error-message-container error"]'),
 	};
 
-	UserOK() {
+	TypeValidUser() {
 		this.get.UserInput().type(UserOK);
 	}
 
-	PassOK() {
+	TypeBlockedUser() {
+		this.get.UserInput().type(UserBlocked);
+	}
+
+	TypeValidPass() {
 		this.get.PassInput().type(PassOK);
 	}
 
-	ButtonLogin() {
+	ClickButtonLogin() {
 		this.get.LoginButton().click();
 	}
 }

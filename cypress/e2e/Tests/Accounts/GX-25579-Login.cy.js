db267e707b09098be618f75645d97786b4ff3fad
rojasfy <rojasfy01@gmail.com>
2023-07-26 11:35:58 -0300

    GX-25579 test(accounts): Todas las pruebas han pasado y la Feature funciona de acuerdo a lo esperado

---
+++ cypress/e2e/Tests/Accounts/GX-25579-Login.cy.js
@@ -0,0 +1,69 @@
+import { HomeSaucePage } from '@pages/GX-25579-login.Page';
+import values from '@data/GX-25579-Login.json';
+
+
+const { baseUrl } = Cypress.env();
+
+
+describe('SwagLabs | Account | Iniciar sesión y BR de Accesos', () => {
+	beforeEach('Precondición: Usuario debe estar situado en la página de Login', () => {
+		cy.visit('/');
+		cy.url().should('contain', 'saucedemo');
+	});
+
+	it('25580 | TC1: Validar iniciar sesión exitosamente cuando se ingresan datos validos', () => {
+		values.datosValidos.forEach(test => {
+			cy.log(`--- Usuario: ${test.username} ---`);
+			cy.visit(baseUrl);
+			HomeSaucePage.typeUsername(test.username);
+			HomeSaucePage.typePassword(test.password);
+			HomeSaucePage.clickLogin();
+			cy.url().should('contain', values.endPoint[0]);
+			HomeSaucePage.elements.itemsContainer().should('be.visible');
+		});
+	});
+
+	it('25580  | TC2: Validar no poder iniciar sesión cuando se ingresa una cuenta bloqueada', () => {
+		const lockedData = values.datosBloqueados;
+
+		HomeSaucePage.typeUsername(lockedData.username);
+		HomeSaucePage.typePassword(lockedData.password);
+		HomeSaucePage.clickLogin();
+		HomeSaucePage.elements.errorMessage().should('have.text', lockedData.expectedMessage);
+		cy.url().should('eql', `${baseUrl}/`);
+	});
+
+	it('25580  | TC3: Validar no poder iniciar sesión cuando se ingresan  datos invalidos', () => {
+		values.datosInvalidos.forEach(test => {
+			cy.log(`--- Caso: ${test.nameCase} ---`);
+			cy.visit(baseUrl);
+			HomeSaucePage.typeUsername(test.username);
+			HomeSaucePage.typePassword(test.password);
+			HomeSaucePage.clickLogin();
+			HomeSaucePage.elements.errorMessage().should('have.text', test.expectedMessage);
+			cy.url().should('eql', `${baseUrl}/`);
+		});
+	});
+
+	it('25580  | TC4: Validar no poder iniciar sesión dejando campos del formulario vacios', () => {
+		values.camposVacios.forEach(test => {
+			cy.log(`--- Caso: ${test.nameCase} ---`);
+			cy.visit(baseUrl);
+			HomeSaucePage.typeUsername(test.username);
+			HomeSaucePage.typePassword(test.password);
+			HomeSaucePage.clickLogin();
+			HomeSaucePage.elements.errorMessage().should('have.text', test.expectedMessage);
+			cy.url().should('eql', `${baseUrl}/`);
+		});
+	});
+
+	it('25580  | TC5: Validar no poder ingresar a un endpoint de la website sin haber iniciado sesión', () => {
+		values.endPoint.forEach(test => {
+			cy.log(`--- End Point: '${baseUrl + test}'---`);
+
+			cy.visit(baseUrl + test, { failOnStatusCode: false });
+			cy.url().should('eql', `${baseUrl}/`);
+			HomeSaucePage.elements.errorMessage().should('have.text', `Epic sadface: You can only access '${test}' when you are logged in.`);
+		});
+	});
+}); 

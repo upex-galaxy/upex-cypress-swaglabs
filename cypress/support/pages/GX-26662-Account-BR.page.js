eccfbe8188ed56933ef5899a77dc458b28ea55cc
ElenaValera <secretodelmar@gmail>
2023-08-02 10:32:43 +0200

    "GX-26662-finishingPOM"

--- cypress/support/pages/GX-26662-Account-BR.page.js
+++ cypress/support/pages/GX-26662-Account-BR.page.js
@@ -1,8 +1,18 @@
 class Account {
 
-    GET = {
+    get = {
+        usernameInput: () => cy.get('#user-name'),
+        passwordInput: () => cy.get('#password'),
+        submitBtn: () => cy.get('#login-button'),
+        errorMessage: () => cy.get('[data-test="error"]'),
 
-
+    };
+    EnterData(username, password) {
+        this.get.usernameInput().type(username);
+        this.get.passwordInput().type(password);
+    }
+    submitClick() {
+        this.get.submitBtn().click();
     }
 }
 

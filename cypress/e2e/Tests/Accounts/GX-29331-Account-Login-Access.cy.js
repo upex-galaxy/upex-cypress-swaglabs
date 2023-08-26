8f0bc9cc6d21fad992d492550f2944777302066e
Brayan Alvarado <balvarado91@gmail.com>
2023-08-26 11:23:25 -0600

    GX-29331-Agregando Test cases TC2-TC3-TC4-TC5-TC6

--- cypress/e2e/Tests/Accounts/GX-29331-Account-Login-Access.cy.js
+++ cypress/e2e/Tests/Accounts/GX-29331-Account-Login-Access.cy.js
@@ -1,27 +1,50 @@
-import {login} from '../../../support/pages/GX-29331-Account/GX-29331-Login.page';
-import credentials from '../../../fixtures/data/GX-29331-My-data.json'
+import { login } from '../../../support/pages/GX-29331-Account/GX-29331-Login.page';
+import credentials from '../../../fixtures/data/GX-29331-My-data.json';
 
 describe('SwagLabs | Account | Iniciar sesión y BR de Accesos', () => {
-    beforeEach(() =>{
-        cy.visit('/');
-    })
+	beforeEach(() => {
+		cy.visit('/');
+	});
 
-    it('29333 | TC1: Verifique que el usuario inicia sesión correctamente', ()=>{
-        login.enterUsername('standard_user');
-        login.enterPassword('secret_sauce');
-        login.clickOnSubmitBtn();
-        login.assertTitlePage();
-    })
+	it('29333 | TC1: Verifique que el usuario inicia sesión correctamente', () => {
+		login.enterUsername('standard_user');
+		login.enterPassword('secret_sauce');
+		login.clickOnSubmitBtn();
+		login.assertTitlePage();
+	});
 
-    // it('29333 | TC2: Verifique que el usuario no pueda iniciar sesión con cuenta bloqueada', ()=>{
-    //     login.enterUsername(data.Credentials.);
-    // })
+	it('29333 | TC2: Verifique que el usuario no pueda iniciar sesión con cuenta bloqueada', () => {
+		login.enterUsername('locked_out_user');
+		login.enterPassword('secret_sauce');
+		login.clickOnSubmitBtn();
+		login.assertMessageBlocked();
+	});
 
-    // it('29333 | TC3: Verifique que el usuario no pueda iniciar sesión con un cuenta incorrecta o inexistente', ()=>{
-    //     login.enterUsername(data.Credentials.);
-    // })
+	it('29333 | TC3: Verifique que el usuario no pueda iniciar sesión con un cuenta incorrecta o inexistente', () => {
+		login.enterUsername('standard_user_not_exist');
+		login.enterPassword('secret_sauce');
+		login.clickOnSubmitBtn();
+		login.assertAccountFailedMessage();
+	});
 
-    // it('29333 | TC4: Verifique que el usuario no pueda iniciar sesión dejando campos vacíos en el formulario', ()=>{
-    //     login.enterUsername(data.Credentials.);
-    // })
+	it('29333 | TC4: Verifique que el usuario no pueda iniciar sesión con un cuenta incorrecta o inexistente', () => {
+		login.enterUsername('standard_user');
+		login.enterPassword('secret_sauce_not_exist');
+		login.clickOnSubmitBtn();
+		login.assertAccountFailedMessage();
+	});
+
+	it('29333 | TC5: Verifique que el usuario no pueda iniciar sesión dejando el username vacío en el formulario', () => {
+		login.emptyUsername();
+		login.enterPassword('secret_password');
+		login.clickOnSubmitBtn();
+		login.assertEmptyUsername();
+	});
+
+	it('29333 | TC6: Verifique que el usuario no pueda iniciar sesión dejando el password vacío en el formulario', () => {
+		login.enterUsername('standard_user');
+		login.emptyPassword();
+		login.clickOnSubmitBtn();
+		login.assertEmptyPassword();
+	});
 });
--- cypress/support/pages/GX-29331-Account/GX-29331-Login.page.js
+++ cypress/support/pages/GX-29331-Account/GX-29331-Login.page.js
@@ -1,27 +1,51 @@
 class Login {
-    elements = {
+	elements = {
 		usernameInput: () => cy.get('[data-test="username"]'),
 		passwordInput: () => cy.get('#password'),
 		submitButton: () => cy.get('#login-button'),
 		dataError: () => cy.get('[data-test="error"]'),
-        swagsWord: () => cy.get('.app_logo')
-    }
+		swagsWord: () => cy.get('.app_logo'),
+	};
 
-    enterUsername(username){
-      this.elements.usernameInput().type(username);
-    }
+	enterUsername(username) {
+		this.elements.usernameInput().type(username);
+	}
 
-    enterPassword(password){
-        this.elements.passwordInput().type(password);
-    }
+	enterPassword(password) {
+		this.elements.passwordInput().type(password);
+	}
 
-    clickOnSubmitBtn(){
-        this.elements.submitButton().click();
-    }
+	clickOnSubmitBtn() {
+		this.elements.submitButton().click();
+	}
 
-    assertTitlePage(){
-        this.elements.swagsWord().should('exist');
-    }
+	emptyUsername() {
+		this.elements.usernameInput().invoke('val', '');
+	}
+
+	emptyPassword() {
+		this.elements.passwordInput().invoke('val', '');
+	}
+
+	assertTitlePage() {
+		this.elements.swagsWord().should('exist');
+	}
+
+	assertMessageBlocked() {
+		this.elements.dataError().should('contain', 'Epic sadface: Sorry, this user has been locked out.');
+	}
+
+	assertAccountFailedMessage() {
+		this.elements.dataError().should('contain', 'Epic sadface: Username and password do not match any user in this service');
+	}
+
+	assertEmptyUsername() {
+		this.elements.dataError().should('contain', 'Epic sadface: Username is required');
+	}
+
+    assertEmptyPassword() {
+		this.elements.dataError().should('contain', 'Epic sadface: Password is required');
+	}
 
 }
 export const login = new Login();

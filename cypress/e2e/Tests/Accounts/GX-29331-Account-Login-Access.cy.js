46bcbc2a594ddb49d980ce9247598a0083c2dfd3
Brayan Alvarado <balvarado91@gmail.com>
2023-08-28 10:10:16 -0600

    GX-29331-Adding Fixture File to improve the best practices of the code

--- cypress/e2e/Tests/Accounts/GX-29331-Account-Login-Access.cy.js
+++ cypress/e2e/Tests/Accounts/GX-29331-Account-Login-Access.cy.js
@@ -1,48 +1,53 @@
 import { login } from '../../../support/pages/GX-29331-Account/GX-29331-Login.page';
 import credentials from '../../../fixtures/data/GX-29331-My-data.json';
 
+const validData = credentials.validDataFixture;
+const blockedData = credentials.blockedDataFixture;
+const invalidUsernameData = credentials.invalidUsernameFixture;
+const invalidPasswordData = credentials.invalidPasswordFixture;
+
 describe('SwagLabs | Account | Iniciar sesión y BR de Accesos', () => {
 	beforeEach(() => {
 		cy.visit('/');
 	});
 
 	it('29333 | TC1: Verifique que el usuario inicia sesión correctamente', () => {
-		login.enterUsername('standard_user');
-		login.enterPassword('secret_sauce');
+		login.enterUsername(validData.username);
+		login.enterPassword(validData.password);
 		login.clickOnSubmitBtn();
 		login.assertTitlePage();
 	});
 
 	it('29333 | TC2: Verifique que el usuario no pueda iniciar sesión con cuenta bloqueada', () => {
-		login.enterUsername('locked_out_user');
-		login.enterPassword('secret_sauce');
+		login.enterUsername(blockedData.usernameBlocked);
+		login.enterPassword(validData.password);
 		login.clickOnSubmitBtn();
 		login.assertMessageBlocked();
 	});
 
-	it('29333 | TC3: Verifique que el usuario no pueda iniciar sesión con un cuenta incorrecta o inexistente', () => {
-		login.enterUsername('standard_user_not_exist');
-		login.enterPassword('secret_sauce');
+	it('29333 | TC3: Verifique que el usuario no pueda iniciar sesión con una cuenta incorrecta o inexistente', () => {
+		login.enterUsername(invalidUsernameData.usernameInvalid);
+		login.enterPassword(validData.password);
 		login.clickOnSubmitBtn();
 		login.assertAccountFailedMessage();
 	});
 
-	it('29333 | TC4: Verifique que el usuario no pueda iniciar sesión con un cuenta incorrecta o inexistente', () => {
-		login.enterUsername('standard_user');
-		login.enterPassword('secret_sauce_not_exist');
+	it('29333 | TC4: Verifique que el usuario no pueda iniciar sesión con un password incorrecto o inexistente', () => {
+		login.enterUsername(validData.username);
+		login.enterPassword(invalidPasswordData.passwordInvalid);
 		login.clickOnSubmitBtn();
 		login.assertAccountFailedMessage();
 	});
 
 	it('29333 | TC5: Verifique que el usuario no pueda iniciar sesión dejando el username vacío en el formulario', () => {
 		login.emptyUsername();
-		login.enterPassword('secret_password');
+		login.enterPassword(validData.password);
 		login.clickOnSubmitBtn();
 		login.assertEmptyUsername();
 	});
 
 	it('29333 | TC6: Verifique que el usuario no pueda iniciar sesión dejando el password vacío en el formulario', () => {
-		login.enterUsername('standard_user');
+		login.enterUsername(validData.username);
 		login.emptyPassword();
 		login.clickOnSubmitBtn();
 		login.assertEmptyPassword();

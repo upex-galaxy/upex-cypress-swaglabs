845491fd51ca1c75c02d0395a46bc5cbeb4b83be
Lucas Ramirez <rlucasmanuel@gmail.com>
2023-09-15 11:15:29 -0300

    GX-30139 TC04, in progress

--- cypress/e2e/Tests/Accounts/GX-30139-Account-Login-Access.cy.js
+++ cypress/e2e/Tests/Accounts/GX-30139-Account-Login-Access.cy.js
@@ -1,14 +1,54 @@
+import { forEach } from 'cypress/types/lodash';
 import { login } from '../../../support/pages/GX-30139-Account-Login';
 
 describe('TS34140 | ✅SwagLabs | Account | Iniciar sesión y BR de Accesos', () => {
 	beforeEach('Precondicion: The user has register and stay in login page', () => {
 		cy.fixture('data/GX-30139-Account-Login').then(data => {
-			cy.visit(data.url.loginPage + data.endpoints.login);
-			cy.url().should('include', 'login');
+			cy.visit(data.url.loginPage);
 		});
 	});
-	it('34140 | TC01: Validate Log in with username “standard_user” and valid password', () => {
-		cy.log('hola');
+	it('34140 | TC01: Validate Log-in with username “standard_user” and valid password', () => {
+		cy.fixture('data/GX-30139-Account-Login').then(data => {
+			login.typeUsername(data.credentials.username.valid[0]);
+			login.typePassword(data.credentials.password.valid);
+			login.clickLoginButton();
+			cy.url().should('include', data.endpoints.inventory);
+			login.get.imgItems().should('be.visible');
+		});
+	});
+	it.only('34140 | TC02: Validate Log in with username “problem_user” and valid password', () => {
+		cy.fixture('data/GX-30139-Account-Login').then(data => {
+			login.typeUsername(data.credentials.username.valid[1]);
+			login.typePassword(data.credentials.password.valid);
+			login.clickLoginButton();
+			cy.url().should('include', data.endpoints.inventory);
+			login.get.imgItems().should('be.visible');
+			login.get.imgItems().then(elm => {
+				Cypress.env('longitud', elm.length);
+				for (let i = 0; i < Cypress.env('longitud'); i = i + 1) {
+					cy.log(login.get.imgItems().eq(i).invoke('attr', 'src'));
+				}
+				cy.log(login.get.imgItems().eq(0).invoke('attr', 'src'));
+			});
+		});
+	});
+	it('34140 | TC03: Validate Log in with username “performance_glitch_user” and valid password', () => {
+		cy.fixture('data/GX-30139-Account-Login').then(data => {
+			login.typeUsername(data.credentials.username.blocked);
+			login.typePassword(data.credentials.password.valid);
+			login.clickLoginButton();
+			login.get.errorMessage().should('have.text', data.errorMessage.usernameBlocked);
+			cy.url().should('not.include', data.endpoints.inventory);
+		});
+	});
+	it('34140 | TC04: Validate dont Log in with locked username', () => {
+		cy.fixture('data/GX-30139-Account-Login').then(data => {
+			login.typeUsername(data.credentials.username.blocked);
+			login.typePassword(data.credentials.password.valid);
+			login.clickLoginButton();
+			login.get.errorMessage().should('have.text', data.errorMessage.usernameBlocked);
+			cy.url().should('not.include', data.endpoints.inventory);
+		});
 	});
 });
 import { removeLogs } from '@helper/RemoveLogs';
--- cypress/support/pages/GX-30139-Account-Login.js
+++ cypress/support/pages/GX-30139-Account-Login.js
@@ -1,8 +1,10 @@
 class loginPage {
 	get = {
-		usernameInput: () => cy.get('#username'),
+		usernameInput: () => cy.get('#user-name'),
 		passwordInput: () => cy.get('#password'),
 		loginButton: () => cy.get('#login-button'),
+		imgItems: () => cy.get('img.inventory_item_img'),
+		errorMessage: () => cy.get('div.error-message-container.error'),
 	};
 	typeUsername(username) {
 		this.get.usernameInput().type(username);

650c5501f0c97aa3c9b58a48552d50658ce4beaf
rafo18 <rafaelbarreto10@hotmail.com>
2023-09-20 00:10:51 -0400

    GX-34846: Se realizo el segundo TC donde se valido agregar producto desde el PDP, tambien se creo el POM del PDP y se modifico el POM del PLP

--- cypress/support/pages/GX-34846-agregarProductosAlCart/GX-34846-PLP.js
+++ cypress/support/pages/GX-34846-agregarProductosAlCart/GX-34846-PLP.js
@@ -4,7 +4,7 @@ class Plp {
 		title: () => cy.get('[class=inventory_item_name]'),
 		description: () => cy.get('[class=inventory_item_desc]'),
 		price: () => cy.get('[class=inventory_item_price]'),
-		cardBtn: () => cy.get('[class*=btn_primary ]'),
+		cardBtn: () => cy.get('[class*=btn_inventory ]'),
 		cartIcon: () => cy.get('[class=shopping_cart_link]'),
 	};
 
@@ -39,6 +39,43 @@ class Plp {
 					});
 
 				this.get.cardBtn().eq(randomNumber).click();
+			})
+			.then(() => {
+				return [title, desc, price, randomNumber];
+			});
+	}
+
+	clickRandomCard() {
+		let title;
+		let desc;
+		let price;
+		let randomNumber;
+
+		return this.get
+			.cardContainer()
+			.then(index => {
+				randomNumber = Cypress._.random(0, index.length - 1);
+
+				this.get
+					.title()
+					.eq(randomNumber)
+					.then($title => {
+						title = $title.text();
+					});
+				this.get
+					.description()
+					.eq(randomNumber)
+					.then($des => {
+						desc = $des.text();
+					});
+				this.get
+					.price()
+					.eq(randomNumber)
+					.then($price => {
+						price = $price.text();
+					});
+
+				this.get.title().eq(randomNumber).click();
 			})
 			.then(() => {
 				return [title, desc, price, randomNumber];

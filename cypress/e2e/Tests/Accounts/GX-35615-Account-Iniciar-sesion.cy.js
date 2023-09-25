571db774b4038fc43dd4978d396c4380c7ea1829
Yesi <109559305+yeyu2083@users.noreply.github.com>
2023-09-24 23:39:02 -0300

    GX-35615 Realizo TC3,4,5,6, 7 y 8

--- cypress/e2e/Tests/Accounts/GX-35615-Account-Iniciar-sesion.cy.js
+++ cypress/e2e/Tests/Accounts/GX-35615-Account-Iniciar-sesion.cy.js
@@ -9,7 +9,6 @@ describe('Precondición: Usuario debe estar situado en la página de Login', () 
 		cy.url().should('contain', 'saucedemo');
 		cy.fixture('data/GX-35615-Login').then(fixtureData => {
 			data = fixtureData;
-			cy.log(data);
 		});
 	});
 	it('35616 | TC1: Validar poder ingresar al Login con user standar_user con password válida', () => {
@@ -19,6 +18,7 @@ describe('Precondición: Usuario debe estar situado en la página de Login', () 
 		loginAccount.submitLogin();
 
 		cy.url().should('include', '/inventory.html');
+		loginAccount.get.item().should('be.visible');
 	});
 
 	it('35616 | TC2: Validar no poder ingresar al Login con user locked_out_user con password válida', () => {
@@ -26,8 +26,64 @@ describe('Precondición: Usuario debe estar situado en la página de Login', () 
 		loginAccount.setUser(username.data.invalid_locked);
 		loginAccount.setPass(password.data.valid);
 		loginAccount.submitLogin();
-
+		loginAccount.errMsg();
+		loginAccount.get.text_error().should('include.text', messageError.data.text_error4);
+		loginAccount.setButton();
+	});
+	it('35616 | TC3: Validar ingresar al Login con user problem_user con password válida', () => {
+		const { username, password } = data;
+		loginAccount.setUser(username.data.invalid_problem);
+		loginAccount.setPass(password.data.valid);
+		loginAccount.submitLogin();
+		cy.url().should('contain', '/inventory.html');
+	});
+	it('35616 | TC4: Validar no poder ingresar al Login con user perfomance_glitch_user con password válida', () => {
+		const { username, password, messageError } = data;
+		loginAccount.setUser(username.data.invalid_perfomance);
+		loginAccount.setPass(password.data.valid);
+		loginAccount.submitLogin();
+		loginAccount.errMsg();
+		loginAccount.get.text_error().should('include.text', messageError.data.text_error1);
+		cy.get('body').should('not.contain', '/inventory.html');
+	});
+	it('35616 | TC5: Validar  no poder ingresar al Login con campo vacío para username', () => {
+		const { messageError } = data;
+		loginAccount.submitLogin();
 		loginAccount.errMsg();
 		loginAccount.get.text_error().should('include.text', messageError.data.text_error3);
+		loginAccount.setButton();
+
+		cy.get('body').should('not.contain', '/inventory.html');
+	});
+	it('35616 | TC6: Validar  no poder ingresar al Login con campo vacío para password', () => {
+		const { username, messageError } = data;
+		loginAccount.setUser(username.data.valid);
+		loginAccount.submitLogin();
+		loginAccount.errMsg();
+		loginAccount.get.text_error().should('include.text', messageError.data.text_error2);
+		loginAccount.setButton();
+
+		cy.get('body').should('not.contain', '/inventory.html');
+	});
+	it('35616 | TC7: Validar no poder ingresar al Login con user standar_user con password inválida', () => {
+		const { username, password, messageError } = data;
+		loginAccount.setUser(username.data.valid);
+		loginAccount.setPass(password.data.invalid);
+		loginAccount.submitLogin();
+		loginAccount.errMsg();
+		loginAccount.get.text_error().should('include.text', messageError.data.text_error1);
+		loginAccount.setButton();
+		cy.get('body').should('not.contain', '/inventory.html');
+	});
+	it('35616 | TC8: Validar no poder ingresar al Login con user problem_user con password inválida', () => {
+		const { username, password, messageError } = data;
+		loginAccount.setUser(username.data.invalid_problem);
+		loginAccount.setPass(password.data.invalid);
+		loginAccount.submitLogin();
+		loginAccount.errMsg();
+		loginAccount.get.text_error().should('include.text', messageError.data.text_error1);
+		loginAccount.setButton();
+
+		cy.get('body').should('not.contain', '/inventory.html');
 	});
 });

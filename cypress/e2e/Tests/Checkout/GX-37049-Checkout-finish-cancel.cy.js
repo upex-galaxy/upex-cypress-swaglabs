bf375e36e93ba0a636b12ccfb105f5a8e50a68f7
PabloCz <Pablotest@gmail.com>
2023-09-25 21:26:58 -0300

    GX-37049 Config inicial y GX-1234 Merge QA Into GX-1234

---
+++ cypress/e2e/Tests/Checkout/GX-37049-Checkout-finish-cancel.cy.js
@@ -0,0 +1,9 @@
+describe('SwagLabs | Checkout | Finalizar o Cancelar la compra de un producto en la Website', () => {
+	beforeEach(
+		'PCR: Situarse en la Web , Loguearse correctamente , add 1 producto o más, realizar proceso checkout , estar situado en la confirm del product',
+		() => {
+			cy.visit('/');
+		}
+	);
+	it('bro', () => {});
+});

fd693e867b1584b6bcac686b8a83403b40575664
Daryelin Rossell <daryelin.rossell@gmail.com>
2023-10-25 14:23:09 -0400

    GX-40919 moviendo los elementos del fixture al constructor del POM

--- cypress/e2e/Tests/SCP/GX-40919-AccederAlShoppingCartYvisualizarContenido.cy.js
+++ cypress/e2e/Tests/SCP/GX-40919-AccederAlShoppingCartYvisualizarContenido.cy.js
@@ -11,37 +11,28 @@ const fillFormAndSubmit = (userName, password, valueAssert) => {
 		shoppingCartPage.get.elementInSwaglabs().should('have.text', valueAssert);
 	}
 };
-const shouldByText = (elemento, texto) => {
-	cy.get(elemento).should('have.text', texto);
-};
-const shouldById = (elemento, id) => {
-	cy.get(elemento).should('have.id', id);
-};
-const clickIn = elemento => {
-	cy.get(elemento).click();
-};
 
 describe('GX-40919 | SwagLabs | SCP | Acceder al Shopping Cart y visualizar contenido', () => {
 	beforeEach('PRC:visit login SwagLab y loguearse', () => {
-		shoppingCartPage.get.endpoint();
+		cy.visit('https://www.saucedemo.com/');
 		fillFormAndSubmit(data.username, data.password, data.title);
 	});
 	it('40920 | TC1: Validar acceder al carrito vacío)', () => {
 		shoppingCartPage.clickIconShoppingCart();
-		shouldByText(data.element.titleCartEmpty, data.text.titleCartEmpty);
-		shouldByText(data.element.quantityColumn, data.text.quantityColumn);
-		shouldByText(data.element.descriptionColumn, data.text.descriptionColumn);
-		shouldById(data.element.buttonContinueShopping, data.id.continueShopping);
-		shouldById(data.element.buttonCheckout, data.id.buttonCheckout);
+		shoppingCartPage.titleCartEmptyAssert();
+		shoppingCartPage.quantityColumnAssert();
+		shoppingCartPage.descriptionColumnAssert();
+		shoppingCartPage.buttonContinueShoppingAssert();
+		shoppingCartPage.buttonCheckoutAssert();
 	});
-	it.only('40920 | TC2: Validar acceder al carrito lleno)', () => {
-		clickIn(data.element.buttonAddCard);
-		shouldByText(data.element.buttonRemove, data.text.buttonRemove);
-		clickIn(data.element.shoppingCartLink);
-		shouldByText(data.element.inventoryItemName, data.text.inventoryItemName);
-		shouldByText(data.element.shoppingcartBadge, data.text.numShoppingcartBadge);
-		cy.get('.cart_quantity').should('have.attr', 'class', 'cart_quantity');
-		shouldByText(data.element.inventoryItemDesc, data.text.inventoryItemDesc);
-		shouldByText(data.element.inventoryItemPrice, data.text.inventoryItemPrice);
+	it('40920 | TC2: Validar acceder al carrito lleno)', () => {
+		shoppingCartPage.clickInButtonAddCard();
+		shoppingCartPage.buttonRemoveAssert();
+		shoppingCartPage.clickInShoppingCartLink();
+		shoppingCartPage.inventoryItemNameAssert();
+		shoppingCartPage.shoppingcartBadgeAssert();
+		shoppingCartPage.cartQuantityAssert();
+		shoppingCartPage.inventoryItemDescAssert();
+		shoppingCartPage.inventoryItemPriceAssert();
 	});
 });
--- cypress/support/pages/GX-40919-AccederAlShoppingCartYvisualizarContenido.page.js
+++ cypress/support/pages/GX-40919-AccederAlShoppingCartYvisualizarContenido.page.js
@@ -1,7 +1,6 @@
 import data from '@data/GX-40919-AccederAlShoppingCartYvisualizarContenido.json';
 class ShoppingCartPage {
 	get = {
-		endpoint: () => cy.visit('https://www.saucedemo.com/'),
 		username: () => cy.get('#user-name'),
 		password: () => cy.get('#password'),
 		loginButton: () => cy.get('#login-button'),
@@ -15,6 +14,12 @@ class ShoppingCartPage {
 		buttonCheckout: () => cy.get('#checkout'),
 		buttonAddCard: () => cy.get('#add-to-cart-sauce-labs-backpack'),
 		buttonRemove: () => cy.get('#remove-sauce-labs-backpack'),
+		shoppingCartLink: () => cy.get('.shopping_cart_link'),
+		inventoryItemName: () => cy.get('.inventory_item_name'),
+		shoppingcartBadge: () => cy.get('.shopping_cart_badge'),
+		cartQuantity: () => cy.get('.cart_quantity'),
+		inventoryItemDesc: () => cy.get('.inventory_item_desc'),
+		inventoryItemPrice: () => cy.get('.inventory_item_price'),
 	};
 
 	typeUsername() {
@@ -26,8 +31,51 @@ class ShoppingCartPage {
 	clickLoginButton() {
 		this.get.loginButton().click();
 	}
+
 	clickIconShoppingCart() {
 		this.get.iconShoppingCart().click();
+	}
+
+	buttonRemoveAssert() {
+		this.get.buttonRemove().should('have.text', data.text.buttonRemove);
+	}
+
+	clickInButtonAddCard() {
+		this.get.buttonAddCard().click();
+	}
+	clickInShoppingCartLink() {
+		this.get.shoppingCartLink().click();
+	}
+	titleCartEmptyAssert() {
+		this.get.titleCartEmpty().should('have.text', data.text.titleCartEmpty);
+	}
+	quantityColumnAssert() {
+		this.get.quantityColumn().should('have.text', data.text.quantityColumn);
+	}
+	descriptionColumnAssert() {
+		this.get.descriptionColumn().should('have.text', data.text.descriptionColumn);
+	}
+	buttonContinueShoppingAssert() {
+		this.get.buttonContinueShopping().should('have.id', 'continue-shopping');
+	}
+	buttonCheckoutAssert() {
+		this.get.buttonCheckout().should('have.id', 'checkout');
+	}
+
+	inventoryItemNameAssert() {
+		this.get.inventoryItemName().should('have.text', data.text.inventoryItemName);
+	}
+	shoppingcartBadgeAssert() {
+		this.get.shoppingcartBadge().should('have.text', data.text.numShoppingcartBadge);
+	}
+	cartQuantityAssert() {
+		this.get.cartQuantity().should('have.attr', 'class', 'cart_quantity');
+	}
+	inventoryItemDescAssert() {
+		this.get.inventoryItemDesc().should('have.text', data.text.inventoryItemDesc);
+	}
+	inventoryItemPriceAssert() {
+		this.get.inventoryItemPrice().should('have.text', data.text.inventoryItemPrice);
 	}
 }
 

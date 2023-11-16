aeb36f0c011522265091fd8767dbeaab65c59fcb
igflores <139146133+igflores@users.noreply.github.com>
2023-11-16 06:42:33 -0800

    Test(GX3-385): Added TC01: Check that the user can log in when providing valid credentials and TC02: Check that the user can not log in when the username is locked', also deleted the file and fixture archive called GX3-835-LoginAndAccessBR.json, because won't be necessary, I'll use the data of cypress.config.js archive

--- cypress/e2e/Tests/Accounts/GX3-385-Login-and-Access-BR.cy.js
+++ cypress/e2e/Tests/Accounts/GX3-385-Login-and-Access-BR.cy.js
@@ -1,12 +1,59 @@
-import { login } from '@pages/GX3-385-LoginAndAccessBRPage/GX3-385-LoginAndAccessBRPage';
+import { loginSwagLabs } from '@pages/GX3-385-LoginAndAccessBRPage/GX3-385-LoginAndAccessBRPage';
 
-// describe('GX3-385 | SwagLabs | Account | Login and sccessBR', () => {
-//     beforeEach(() => {
-// 		cy.visit('/');
-//     });
-    
-//     it('416 | TC01: Check that the user can log in when providing valid credentials', () => {
-		
-// 	});
+describe('GX3-385 | SwagLabs | Account | Login and sccessBR', () => {
+	const { baseUrl } = Cypress.env(); //para acceder a la propiedad de "baseUrl" (ver archivo cypress.config.js)
+	const { login: loginProperties } = Cypress.env('swagLabs'); // significa loginProperties = Cypress.env('swagLabs').login
+	beforeEach('Preconditions: ', () => {
+		cy.visit(baseUrl);
+		cy.url().should('contain', 'saucedemo.com');
+	});
 
-// });
+	it('416 | TC01: Check that the user can log in when providing valid credentials', () => {
+		loginSwagLabs.Login(loginProperties.users.correctUser, loginProperties.users.correctPass);
+		cy.url().should('contain', 'saucedemo.com');
+	});
+
+	it('416 | TC02: Check that the user can not log in when the username is locked', () => {
+		loginSwagLabs.Login(loginProperties.users.lockUser, loginProperties.users.correctPass);
+		cy.url().should('contain', baseUrl);
+		loginSwagLabs.get.notMatchMessage().should('contain', loginProperties.errorMsg.lockedUser);
+	});
+	/*
+	it('416 | TC03: Check that the user can not log in with an incorrect or nonexistent username', () => {
+		loginSwagLabs.Login(loginCredentials.users.correctUser, loginCredentials.users.correctPass);
+		cy.url().should('contain', 'inventory.html');
+	});
+	it('416 | TC04: Check that the user cannot log in with an incorrect or nonexistent password', () => {
+		loginSwagLabs.Login(loginCredentials.users.correctUser, loginCredentials.users.correctPass);
+		cy.url().should('contain', 'inventory.html');
+	});
+	it('416 | TC05: Check that the user can not log in with an incorrect or nonexistent username and password', () => {
+		loginSwagLabs.Login(loginCredentials.users.correctUser, loginCredentials.users.correctPass);
+		cy.url().should('contain', 'inventory.html');
+	});
+	it('416 | TC06: Check that the user can not log in when leaving the username empty in the form', () => {
+		loginSwagLabs.Login(loginCredentials.users.correctUser, loginCredentials.users.correctPass);
+		cy.url().should('contain', 'inventory.html');
+	});
+	it('416 | TC07: Check that the user can not log in when leaving the password empty in the form', () => {
+		loginSwagLabs.Login(loginCredentials.users.correctUser, loginCredentials.users.correctPass);
+		cy.url().should('contain', 'inventory.html');
+	});
+	it('416 | TC08: Check that the user can not log in when leaving the username and password empty in the form', () => {
+		loginSwagLabs.Login(loginCredentials.users.correctUser, loginCredentials.users.correctPass);
+		cy.url().should('contain', 'inventory.html');
+	});
+	it('416 | TC09: Check that the user can not access an endpoint without logging in', () => {
+		loginSwagLabs.Login(loginCredentials.users.correctUser, loginCredentials.users.correctPass);
+		cy.url().should('contain', 'inventory.html');
+	});*/
+});
+
+// NOTA:
+// Se utiliza const { login: loginProperties } = Cypress.env('swagLabs'); para renombrar
+// la propiedad "login" del objeto "Cypress.env('swagLabs') a "loginProperties"
+// para hacer el código más limpio y legible.
+// Otra variante sería utilizar:
+// const loginProperties = Cypress.env('swagLabs');
+// y luego acceder a las propiedades directamente:
+// "loginProperties.login.users.correctUser"  y "loginProperties.login.users.correctPass"
--- cypress/support/pages/GX3-385-LoginAndAccessBRPage/GX3-385-LoginAndAccessBRPage.js
+++ cypress/support/pages/GX3-385-LoginAndAccessBRPage/GX3-385-LoginAndAccessBRPage.js
@@ -1,15 +1,15 @@
-class LoginSwagLab { 
-    get = {
-        userNameInput: () => cy.get('[data-test="username"]'),
-        passwordInput: () => cy.get('data-test="password"'),
-        loginButon: () => cy.get('[data-test="login-button"]'),
-        notMatchMessage: () => cy.get('[data-test="error"]'),
-    };
+class LoginSwagLab {
+	get = {
+		userNameInput: () => cy.get('[data-test="username"]'),
+		passwordInput: () => cy.get('[data-test="password"'),
+		loginButon: () => cy.get('[data-test="login-button"]'),
+		notMatchMessage: () => cy.get('[data-test="error"]'),
+	};
 
-    Login(username, password) { 
-        username && this.get.userNameInput().type(username);
-        password && this.get.passwordInput().type(password);
-        this.get.loginButon().click();
-    }     
+	Login(username, password) {
+		username && this.get.userNameInput().type(username);
+		password && this.get.passwordInput().type(password);
+		this.get.loginButon().click();
+	}
 }
-export const login = new LoginSwagLab();
+export const loginSwagLabs = new LoginSwagLab();

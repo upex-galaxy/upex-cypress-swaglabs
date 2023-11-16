55926d9efbca87aa9f7f2e95486adb9555434d65
igflores <139146133+igflores@users.noreply.github.com>
2023-11-16 11:31:41 -0800

    Test(GX3-385):Added TC09: Check that the user can not access an endpoint without logging in (/inventory.html), TC10: Check that the user can not access an endpoint without logging in (/cart.html) and TC11: Check that the user can not access an endpoint without logging in (/checkout-step-one.html)

--- cypress/e2e/Tests/Accounts/GX3-385-Login-and-Access-BR.cy.js
+++ cypress/e2e/Tests/Accounts/GX3-385-Login-and-Access-BR.cy.js
@@ -2,7 +2,7 @@ import { loginSwagLabs } from '@pages/GX3-385-LoginAndAccessBRPage/GX3-385-Login
 
 describe('GX3-385 | SwagLabs | Account | Login and sccessBR', () => {
 	const { baseUrl } = Cypress.env(); //para acceder a la propiedad de "baseUrl" (ver archivo cypress.config.js)
-	const { login: loginProperties } = Cypress.env('swagLabs'); // significa loginProperties = Cypress.env('swagLabs').login
+	const { login: loginProperties, endpoint } = Cypress.env('swagLabs'); // significa loginProperties = Cypress.env('swagLabs').login
 	beforeEach('Preconditions: ', () => {
 		cy.visit(baseUrl);
 		cy.url().should('contain', 'saucedemo.com');
@@ -54,11 +54,24 @@ describe('GX3-385 | SwagLabs | Account | Login and sccessBR', () => {
 		cy.url().should('contain', baseUrl);
 		loginSwagLabs.get.notMatchMessage().should('contain', loginProperties.errorMsg.UserNull);
 	});
-	/*
-	it('416 | TC09: Check that the user can not access an endpoint without logging in', () => {
-		loginSwagLabs.Login(loginProperties.users.correctUser, loginProperties.users.correctPass);
+
+	it('416 | TC09: Check that the user can not access an endpoint without logging in (/inventory.html)', () => {
+		cy.visit(baseUrl + endpoint.inventory, { failOnStatusCode: false });
 		cy.url().should('contain', baseUrl);
-	});*/
+		loginSwagLabs.get.notMatchMessage().should('contain', loginProperties.errorMsg.inventoryError);
+	});
+
+	it('416 | TC10: Check that the user can not access an endpoint without logging in (/cart.html)', () => {
+		cy.visit(baseUrl + endpoint.cart, { failOnStatusCode: false });
+		cy.url().should('contain', baseUrl);
+		loginSwagLabs.get.notMatchMessage().should('contain', loginProperties.errorMsg.cartError);
+	});
+
+	it('416 | TC11: Check that the user can not access an endpoint without logging in (/checkout-step-one.html)', () => {
+		cy.visit(baseUrl + endpoint.checkoutOne, { failOnStatusCode: false });
+		cy.url().should('contain', baseUrl);
+		loginSwagLabs.get.notMatchMessage().should('contain', loginProperties.errorMsg.checkoutOneError);
+	});
 });
 
 // NOTA:

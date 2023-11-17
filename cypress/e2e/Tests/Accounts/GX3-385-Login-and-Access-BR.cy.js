0b6f51c57626611fd9ca66365e42f085d32933f6
igflores <139146133+igflores@users.noreply.github.com>
2023-11-17 06:58:31 -0800

    Test(GX3-385): Refactoring of code, with Dani's help. Two functions were created to avoid repeating code: fillLoginFormAndGetErrorMessage and visitInvalidEndpoint

--- cypress/e2e/Tests/Accounts/GX3-385-Login-and-Access-BR.cy.js
+++ cypress/e2e/Tests/Accounts/GX3-385-Login-and-Access-BR.cy.js
@@ -1,105 +1,82 @@
 import { loginSwagLabs } from '@pages/GX3-385-LoginAndAccessBRPage/GX3-385-LoginAndAccessBRPage';
+// import { fill } from 'cypress/types/lodash';
+
+const { baseUrl } = Cypress.env(); //para acceder a la propiedad de "baseUrl" (ver archivo cypress.config.js)
+const { login: loginProperties, endpoint } = Cypress.env('swagLabs'); // significa loginProperties = Cypress.env('swagLabs').login
+
+const fillLoginFormAndGetErrorMessage = (userName, password, errorMessage) => {
+	loginSwagLabs.Login(userName, password);
+	cy.url().should('contain', baseUrl);
+	if (errorMessage !== undefined) {
+		loginSwagLabs.get.errorMessageDisplay().should('contain', errorMessage);
+	}
+};
+
+const visitInvalidEndpoint = (endpoint, errorMessage) => {
+	cy.visit(baseUrl + endpoint, { failOnStatusCode: false });
+	cy.url().should('contain', baseUrl);
+	loginSwagLabs.get.errorMessageDisplay().should('contain', errorMessage);
+};
 
 describe('GX3-385 | SwagLabs | Account | Login and sccessBR', () => {
-	const { baseUrl } = Cypress.env(); //para acceder a la propiedad de "baseUrl" (ver archivo cypress.config.js)
-	const { login: loginProperties, endpoint } = Cypress.env('swagLabs'); // significa loginProperties = Cypress.env('swagLabs').login
 	beforeEach('Preconditions: ', () => {
 		cy.visit(baseUrl);
 		cy.url().should('contain', 'saucedemo.com');
 	});
 
 	it('416 | TC01: Check that the user can log in when providing valid credentials', () => {
-		loginSwagLabs.Login(loginProperties.users.correctUser, loginProperties.users.correctPass);
-		cy.url().should('contain', 'saucedemo.com');
+		fillLoginFormAndGetErrorMessage(loginProperties.users.correctUser, loginProperties.users.correctPass);
 	});
 
 	it('416 | TC02: Check that the user can not log in when the username is locked', () => {
-		loginSwagLabs.Login(loginProperties.users.lockUser, loginProperties.users.correctPass);
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.lockedUser);
+		fillLoginFormAndGetErrorMessage(loginProperties.users.lockUser, loginProperties.users.correctPass);
 	});
 
 	it('416 | TC03: Check that the user can not log in with an incorrect or nonexistent username', () => {
-		loginSwagLabs.Login(loginProperties.users.userInv, loginProperties.users.correctPass);
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.PassOrUserInv);
+		fillLoginFormAndGetErrorMessage(loginProperties.users.userInv, loginProperties.users.correctPass);
 	});
 
 	it('416 | TC04: Check that the user cannot log in with an incorrect or nonexistent password', () => {
-		loginSwagLabs.Login(loginProperties.users.correctUser, loginProperties.users.passInv);
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.PassOrUserInv);
+		fillLoginFormAndGetErrorMessage(loginProperties.users.correctUser, loginProperties.users.passInv);
 	});
 
 	it('416 | TC05: Check that the user can not log in with an incorrect or nonexistent username and password', () => {
-		loginSwagLabs.Login(loginProperties.users.userInv, loginProperties.users.passInv);
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.PassOrUserInv);
+		fillLoginFormAndGetErrorMessage(loginProperties.users.userInv, loginProperties.users.passInv);
 	});
 
 	it('416 | TC06: Check that the user can not log in when leaving the username empty in the form', () => {
-		loginSwagLabs.Login('', loginProperties.users.correctPass);
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.UserNull);
+		fillLoginFormAndGetErrorMessage('', loginProperties.users.correctPass);
 	});
 
 	it('416 | TC07: Check that the user can not log in when leaving the password empty in the form', () => {
-		loginSwagLabs.Login(loginProperties.users.correctUser, '');
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.PassNull);
+		fillLoginFormAndGetErrorMessage(loginProperties.users.correctUser, '');
 	});
 
 	it('416 | TC08: Check that the user can not log in when leaving the username and password empty in the form', () => {
-		loginSwagLabs.Login('', '');
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.UserNull);
+		fillLoginFormAndGetErrorMessage('', '');
 	});
 
 	it('416 | TC09: Check that the user can not access an endpoint without logging in (/inventory.html)', () => {
-		cy.visit(baseUrl + endpoint.inventory, { failOnStatusCode: false });
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.inventoryError);
+		visitInvalidEndpoint(endpoint.inventory, loginProperties.errorMsg.inventoryError);
 	});
 
 	it('416 | TC10: Check that the user can not access an endpoint without logging in (/cart.html)', () => {
-		cy.visit(baseUrl + endpoint.cart, { failOnStatusCode: false });
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.cartError);
+		visitInvalidEndpoint(endpoint.cart, loginProperties.errorMsg.cartError);
 	});
 
 	it('416 | TC11: Check that the user can not access an endpoint without logging in (/checkout-step-one.html)', () => {
-		cy.visit(baseUrl + endpoint.checkoutOne, { failOnStatusCode: false });
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.checkoutOneError);
+		visitInvalidEndpoint(endpoint.checkoutOne, loginProperties.errorMsg.checkoutOneError);
 	});
 
 	it('416 | TC12: Check that the user can not access an endpoint without logging in (/checkout-step-two.html)', () => {
-		cy.visit(baseUrl + endpoint.checkoutTwo, { failOnStatusCode: false });
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.checkoutTwoError);
+		visitInvalidEndpoint(endpoint.checkoutTwo, loginProperties.errorMsg.checkoutTwoError);
 	});
 
 	it('416 | TC13: Check that the user can not access an endpoint without logging in (/checkout-complete.html)', () => {
-		cy.visit(baseUrl + endpoint.checkoutAll, { failOnStatusCode: false });
-		cy.url().should('contain', baseUrl);
-		loginSwagLabs.get.errorMessageDisplay().should('contain', loginProperties.errorMsg.checkoutAllError);
+		visitInvalidEndpoint(endpoint.checkoutAll, loginProperties.errorMsg.checkoutAllError);
 	});
 
-	// no veo el mensaje de este caso:
-	// Epic sadface: You can only access '/inventory-item.html' when you are logged in.
-
-	// it('416 | TC14: Check that the user can not access an endpoint without logging in (/inventory-item.html)', () => {
-	// 	cy.visit(baseUrl + endpoint.product, { failOnStatusCode: false });
-	// 	cy.url().should('contain', baseUrl);
-	// 	loginSwagLabs.get.notMatchMessage().should('contain', loginProperties.errorMsg.inventoryError);
-	// });
+	it('416 | TC14: Check that the user can not access an endpoint without logging in (/inventory-item.html)', () => {
+		visitInvalidEndpoint(endpoint.product, loginProperties.errorMsg.inventoryItemError);
+	});
 });
-
-// NOTA:
-// Se utiliza const { login: loginProperties } = Cypress.env('swagLabs'); para renombrar
-// la propiedad "login" del objeto "Cypress.env('swagLabs') a "loginProperties"
-// para hacer el código más limpio y legible.
-// Otra variante sería utilizar:
-// const loginProperties = Cypress.env('swagLabs');
-// y luego acceder a las propiedades directamente:
-// "loginProperties.login.users.correctUser"  y "loginProperties.login.users.correctPass"

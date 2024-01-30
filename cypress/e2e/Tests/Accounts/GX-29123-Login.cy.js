9309ed0ac0fb3d60b4a386a6125adfcd4f7832d0
Marianela Serodino Runco <serodinonela@gmail.com>
2024-01-30 13:18:44 +0000

    GX3-1882 Created TC2 and TC3

--- cypress/e2e/Tests/Accounts/GX-29123-Login.cy.js
+++ cypress/e2e/Tests/Accounts/GX-29123-Login.cy.js
@@ -11,4 +11,12 @@ describe('[Automation] SwagLabs | Account | Iniciar sesión y BR de Accesos', ()
 		login.loginCorrectly(data.validUsernames.standardUser, data.validPassword);
 		login.get.logOutButton().should('exist');
 	});
+	it('49541 | TC2: Check that the user can login with correct username and password (problem_user))', () => {
+		login.loginCorrectly(data.validUsernames.problemUser, data.validPassword);
+		login.get.logOutButton().should('exist');
+	});
+	it('49541 | TC3: Check that the user can login with correct username and password (performance_glitch_user))', () => {
+		login.loginCorrectly(data.validUsernames.performanceUser, data.validPassword);
+		login.get.logOutButton().should('exist');
+	});
 });

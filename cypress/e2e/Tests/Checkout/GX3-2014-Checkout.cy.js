f58c5f39e9a67db10d0abd56c45b10477d54616c
Marianela Serodino Runco <serodinonela@gmail.com>
2024-01-31 12:52:49 +0000

    Created Login.Page file

---
+++ cypress/e2e/Tests/Checkout/GX3-2014-Checkout.cy.js
@@ -0,0 +1,8 @@
+describe('[Automation] SwagLabs | Checkout | Finalizar o Cancelar la compra de un producto en la Website', () => {
+	beforeEach('PRC: The user is logged in, and one product is added to the shopping cart', () => {
+		cy.visit('/');
+	});
+	it('2014 | TC1: Checj that the user can finish a purchase', () => {
+		console.log('hello');
+	});
+});

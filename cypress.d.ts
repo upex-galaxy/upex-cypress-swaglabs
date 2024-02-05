8fa29c2799fe47b9a9e9a1e7171641f212a0d3df
ZwitterKaneda <smentek@gmail.com>
2024-02-05 15:50:43 -0300

    some change

--- cypress.d.ts
+++ cypress.d.ts
@@ -1,13 +1,13 @@
-import { SpaceCheckoutPage } from "@pages/SpaceCheckout.Page"
-import { SpaceLoginPage } from "@pages/SpaceLogin.Page"
-import { SpaceDestinationPage } from "@pages/SpaceProduct.Page"
+import { SpaceCheckoutPage } from '@pages/SpaceCheckout.Page';
+import { SpaceLoginPage } from '@pages/SpaceLogin.Page';
+import { SpaceDestinationPage } from '@pages/SpaceProduct.Page';
 declare global {
     namespace Cypress {
         interface Chainable {
             /**
              * @description
              * El método page() te permite utilizar y encadenar comandos Cypress a los Page Object Models de tu proyecto, lo que facilita la interacción con las páginas de tu aplicación bajo prueba.
-             * @example 
+             * @example
              * cy.page().then(({ spaceLoginPage }) => {
              *    spaceLoginPage.enterUsername('Admin')
              *    spaceLoginPage.enterPassword('admin123')
@@ -20,13 +20,10 @@ declare global {
                 spaceCheckoutPage: SpaceCheckoutPage
             }>
 
-
-
-
             /**
              * @description
              * El método react() te permite utilizar y encadenar comandos Cypress a los elementos de ReactJS de tu aplicación bajo prueba.
-             * @example 
+             * @example
              * cy.react('button', { hasText: 'Log in' }).click()
             */
             react(dataReactToolbox: string, options?: { hasText: string }): Cypress.Chainable<JQuery<HTMLElement>>
